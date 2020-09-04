954d7ec482e11db01f4146120fd64a3c9382a20a
Kadir Gundogdu <kadirgund@gmail.com>
2020-09-04 16:21:49 -0400

    importing helper functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import get from "../functions";
 import * as actions from "../actions/actions";
 import { exec } from "child_process";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
@@ -10,235 +9,24 @@ import Yml from "./tabs/Yml";
 import Running from "./tabs/Running";
 import Stopped from "./tabs/Stopped";
 import { stderr } from "process";
-import parseContainerFormat from './helper/parseContainerFormat';
+import * as helper from "./helper/commands";
+
 
 const App = (props) => {
 	const dispatch = useDispatch();
 	const addRunningContainers = (data) => dispatch(actions.addRunningContainers(data));
-	const removeContainer = (id) => dispatch(actions.removeContainer(id));
-	const stopRunningContainer = (id) => dispatch(actions.stopRunningContainer(id));
 	const addStoppedContainers = (data) => dispatch(actions.addStoppedContainers(data));
-	const runStoppedContainer = (id) => dispatch(actions.runStoppedContainer(id));
 	const addExistingImages = (data) => dispatch(actions.getImages(data))
-
-	// useSelector Allows you to extract data from the Redux store state, using a selector function.
 
 	const runningList = useSelector((state) => state.lists.runningList);
 	const stoppedList = useSelector((state) => state.lists.stoppedList);
 	const imagesList = useSelector((state) => state.lists.imagesList);
 
-
-	// on app start-up, get the containers that are already running by calling addRunning
-	const addRunning = () => {
-		exec("docker stats --no-stream", (error, stdout, stderr) => {
-			if (error) {
-				console.log(`error: ${error.message}`);
-				return;
-			}
-			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-				return;
-			}
-			let value = parseContainerFormat.convert(stdout);
-			let objArray = ['cid', 'name', 'cpu', 'mul', 'mp', 'net', 'block', 'pids'];
-			let convertedValue = parseContainerFormat.convertArrToObj(value, objArray);
-			const newList = []
-			for (let i = 0; i < convertedValue.length; i++) {
-				let isInTheList = false
-				for (let container of runningList) {
-					if (container.id === convertedValue[i].id) {
-						isInTheList = true
-					}
-				}
-				if (!isInTheList) {
-					newList.push(convertedValue[i])
-				}
-			}
-			if (newList.length > 0) {
-				addRunningContainers(newList)
-			}
-		});
-	};
-
-	const addStopped = () => {
-		exec('docker ps -f "status=exited"', (error, stdout, stderr) => {
-			if (error) {
-				console.log(`error: ${error.message}`);
-				return;
-			}
-			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-				return;
-			}
-			let value = parseContainerFormat.convert(stdout);
-			//value => array
-			const result = [];
-			//tempoary we will have it as manual number
-			for (let i = 0; i < value.length; i++) {
-				let innerArray = [];
-				innerArray.push(value[i][0]); // id
-				innerArray.push(value[i][1]); // image
-				if (!isNaN(parseInt(value[i][3]))) {
-					innerArray.push(value[i][3] + ' days ago'); // created 
-				} else {
-					innerArray.push(value[i][6] + ' days ago');
-				}
-				result.push(innerArray);
-			}
-			let objArray = ['cid', 'img', 'created'];
-			let convertedValue = parseContainerFormat.convertArrToObj(result, objArray);
-
-			const newList = []
-			for (let i = 0; i < convertedValue.length; i++) {
-				let isInTheList = false
-				for (let container of stoppedList) {
-					if (container.id === convertedValue[i].id) {
-						isInTheList = true
-					}
-				}
-				if (!isInTheList) {
-					newList.push(convertedValue[i])
-				}
-			}
-			if (newList.length > 0) {
-				addStoppedContainers(newList)
-			}
-		});
-	};
-
-	const addImages = () => {
-		exec(`docker images`, (error, stdout, stderr) => {
-			if (error) {
-				console.log(`error: ${error.message}`);
-				return;
-			}
-			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-				return;
-			}
-			let value = parseContainerFormat.convert(stdout);
-			let objArray = ['reps', 'tag', 'imgid', 'size']
-			const resultImages = [];
-			for (let i = 0; i < value.length; i++) {
-				let innerArray = [];
-				if (value[i][0] !== '<none>') {
-					innerArray.push(value[i][0])
-					innerArray.push(value[i][1])
-					innerArray.push(value[i][2])
-					innerArray.push(value[i][6])
-					resultImages.push(innerArray)
-				}
-			}
-			let convertedValue = parseContainerFormat.convertArrToObj(resultImages, objArray);
-			const newList = []
-			for (let i = 0; i < convertedValue.length; i++) {
-				let isInTheList = false
-				for (let container of imagesList) {
-					if (container.id === convertedValue[i].id) {
-						isInTheList = true
-					}
-				}
-				if (!isInTheList) {
-					newList.push(convertedValue[i])
-				}
-			}
-			if (newList.length > 0) {
-				addExistingImages(newList)
-			}
-		});
-	}
-
-	const remove = (id) => {
-		exec(`docker rm --force ${id}`, (error, stdout, stderr) => {
-			if (error) {
-				console.log(`error: ${error.message}`);
-				return;
-			}
-			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-				return;
-			}
-			
-			removeContainer(id);
-		});
-	};
-
-	const stop = (id) => {
-		exec(`docker stop ${id}`, (error, stdout, stderr) => {
-			if (error) {
-				console.log(`error: ${error.message}`);
-				return;
-			}
-			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-				return;
-			}
-			stopRunningContainer(id);
-		});
-	};
-
-	const runStopped = (id) => {
-		exec(`docker start ${id}`, (error, stdout, stderr) => {
-			if (error) {
-				console.log(`error: ${error.message}`);
-				return;
-			}
-			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-				return;
-			}
-			console.log(stdout)
-			runStoppedContainer(id);
-		});
-	};
-
-	const runIm = (id) => {
-		exec(`docker run ${id}`, (error, stdout, stderr) => {
-			if (error) {
-				console.log(`error: ${error.message}`);
-				return;
-			}
-			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-				return;
-			}
-			runImage(id);
-		});
-	}
-
-	const removeIm = (id) => {
-		exec(`docker rmi -f ${id}`, (error, stdout, stderr) => {
-			if (error) {
-				console.log(`error: ${error.message}`);
-				return;
-			}
-			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-				return;
-			}
-			removeImage(id);
-		});
-	}
-
-	// useEffect(() => {
-	// 	addRunning();
-	// }, [runningList]);
-
-	// useEffect(() => {
-	// 	addStopped();
-	// }, [stoppedList]);
-
-	// useEffect(() => {
-	// 	addImages();
-	// }, [imagesList]);
-
 	useEffect(() => {
-		addRunning();
-		addStopped();
-		addImages();
+		helper.addRunning(runningList, addRunningContainers);
+		helper.addStopped(stoppedList, addStoppedContainers);
+		helper.addImages(imagesList, addExistingImages);
 	}, [])
-
-
 
 	return (
 		<Router>
@@ -276,10 +64,10 @@ const App = (props) => {
 						<Images />
 					</Route>
 					<Route path="/stopped">
-						<Stopped runStopped={runStopped} remove={remove} />
+						<Stopped runStopped={helper.runStopped} remove={helper.remove} />
 					</Route>
 					<Route path="/">
-						<Running stop={stop} />
+						<Running stop={helper.stop} />
 					</Route>
 				</Switch>
 			</div>
--- src/components/tabs/Stopped.js
+++ src/components/tabs/Stopped.js
@@ -1,39 +1,42 @@
 import React from 'react';
 import StoppedContainers from '../display/StoppedContainers';
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from "../../actions/actions";
 
 
 const Stopped = (props) => {
+	const dispatch = useDispatch();
+	const removeContainer = (id) => dispatch(actions.removeContainer(id));
+	const runStoppedContainer = (id) => dispatch(actions.runStoppedContainer(id));
+	const stoppedList = useSelector((state) => state.lists.stoppedList);
 
-    const stoppedList = useSelector((state) => state.lists.stoppedList);
+	const stopTesting = stoppedList.map(ele => {
+		return (
+			<div>
 
-    const stopTesting = stoppedList.map(ele => {
-        return (
-            <div>
-            
-            <ul>ContainerId : {ele['cid']}</ul>
-            <ul>Img: {ele['img']}</ul>
-            <ul>Created: {ele['created']}</ul>
-            <button onClick={() => props.runStopped(ele['cid']) }>RUN</button>
-            <button onClick={() => props.remove(ele['cid'])}>REMOVE</button>
-    
-            </div>
-    
-    
-            )
-        });
+				<ul>ContainerId : {ele['cid']}</ul>
+				<ul>Img: {ele['img']}</ul>
+				<ul>Created: {ele['created']}</ul>
+				<button onClick={() => props.runStopped(ele['cid'], runStoppedContainer)}>RUN</button>
+				<button onClick={() => props.remove(ele['cid'], removeContainer)}>REMOVE</button>
 
-    return (
-        <div>
-            I am in Stopped Hello!!! This is going to render
-            <div>
-            <StoppedContainers />
-            </div>
-            <div>
-            {stopTesting}
-            </div>    
-        </div>
-    )
+			</div>
+
+
+		)
+	});
+
+	return (
+		<div>
+			I am in Stopped Hello!!! This is going to render
+			<div>
+				<StoppedContainers />
+			</div>
+			<div>
+				{stopTesting}
+			</div>
+		</div>
+	)
 }
 
 export default Stopped;

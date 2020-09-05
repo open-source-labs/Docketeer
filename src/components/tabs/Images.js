55d52eb0e0054a6cb191aad4132c7c924f439d6f
Kadir Gundogdu <kadirgund@gmail.com>
2020-09-05 00:14:00 -0400

    fixed bug to allow running containers from images

--- src/components/tabs/Images.js
+++ src/components/tabs/Images.js
@@ -4,35 +4,34 @@ import * as actions from "../../actions/actions";
 import * as helper from "../helper/commands"
 const Images = (props) => {
 
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const runImage = useDispatch((id) => dispatch(actions.runImage(id)));
-    const addRunningContainers = (data) => dispatch(actions.addRunningContainers(data));
+	const addRunningContainers = (data) => dispatch(actions.addRunningContainers(data));
 
-    const imagesList = useSelector((state) => state.lists.imagesList);
-    const runningList = useSelector((state) => state.lists.runningList);
+	const imagesList = useSelector((state) => state.lists.imagesList);
+	const runningList = useSelector((state) => state.lists.runningList);
 	// const addAdditionalImageContainer = (id) => dispatch(actions.runImages(id))
-    const renderImagesList = imagesList.map(ele => {
-        //      let objArray = ['resp', 'tag', 'imgid', 'created', 'size'] 
+	const renderImagesList = imagesList.map(ele => {
+		//      let objArray = ['resp', 'tag', 'imgid', 'created', 'size'] 
 
-        return (
-            <div>
-             <ul>Repository : {ele['reps']}</ul>
-             <ul>Tag : {ele['tag']}</ul>
-             <ul>Image ID : {ele['imgid']}</ul>
-             <ul>Size : {ele['size']}</ul>
-             <button onClick={() => props.runIm(ele['imgid'], runningList, helper.addRunning ,addRunningContainers )}>Add Container based on Image</button>
-            </div>
-        )
-    });
+		return (
+			<div>
+				<ul>Repository : {ele['reps']}</ul>
+				<ul>Tag : {ele['tag']}</ul>
+				<ul>Image ID : {ele['imgid']}</ul>
+				<ul>Size : {ele['size']}</ul>
+				<button onClick={() => props.runIm(ele['imgid'], runningList, helper.addRunning, addRunningContainers)}>Add Container based on Image</button>
+			</div>
+		)
+	});
 
-    return (
-        <div>
-            <div className="renderContainers">
-                {renderImagesList}
-            </div>
-        </div>
-    )
+	return (
+		<div>
+			<div className="renderContainers">
+				{renderImagesList}
+			</div>
+		</div>
+	)
 }
 
 export default Images;

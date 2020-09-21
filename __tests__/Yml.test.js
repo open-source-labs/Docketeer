7c8fd4d1d59abf19a4221278e7fd7e44560a9147
MinchanJun <minchan@Minchans-MacBook-Pro.local>
2020-09-21 10:47:30 -0400

    Metrics test done, but yml test is not done. I left it as commented It will show as failed in the test

--- __tests__/Yml.test.js
+++ __tests__/Yml.test.js
@@ -1,58 +1,58 @@
-import React from 'react';
-import { configure, shallow, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-// import toJson from 'enzyme-to-json';
-import * as Redux from 'react-redux';
-import configureMockStore from 'redux-mock-store';
-import Yml from '../src/components/tabs/Yml';
+// import React from 'react';
+// import { configure, shallow, mount } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// // import toJson from 'enzyme-to-json';
+// import * as Redux from 'react-redux';
+// import configureMockStore from 'redux-mock-store';
+// import Yml from '../src/components/tabs/Yml';
 
-// Newer Enzyme versions require an adapter to a particular version of React
-configure({ adapter: new Adapter() });
+// // Newer Enzyme versions require an adapter to a particular version of React
+// configure({ adapter: new Adapter() });
 
-function shallowSetup() {
+// function shallowSetup() {
 
-    const props = {
-        networkList:     [{
-            "a": [
-                {"cid": "1", "name": "conatiner1"},
-                {"cid": "2", "name": "container2"}
-                ]
-        },
-        {
-            "b": [
-                {"cid": "3", "name": "container3"},
-                {"cid": "4", "name": "container4"}
-                ]
-        }],
-        fileList: 'hello'
-    }
+//     const props = {
+//         networkList:     [{
+//             "a": [
+//                 {"cid": "1", "name": "conatiner1"},
+//                 {"cid": "2", "name": "container2"}
+//                 ]
+//         },
+//         {
+//             "b": [
+//                 {"cid": "3", "name": "container3"},
+//                 {"cid": "4", "name": "container4"}
+//                 ]
+//         }],
+//         fileList: 'hello'
+//     }
 
-  const enzymeWrapper = mount(<Yml {...props} composeymlFiles={jest.fn()}  setFilepath={jest.fn()} filepath={'Hello'} fileList={'hi'} setfileList={jest.fn()} />);
+//   const enzymeWrapper = mount(<Yml {...props} composeymlFiles={jest.fn()}  setFilepath={jest.fn()} filepath={'Hello'} fileList={'hi'} setfileList={jest.fn()} />);
 
-  return {
-    props,
-    enzymeWrapper
-  };
-}
+//   return {
+//     props,
+//     enzymeWrapper
+//   };
+// }
 
-describe('Shallow rendered for Docker Compose Up', () => {
-    let wrapper;     
+// describe('Shallow rendered for Docker Compose Up', () => {
+//     let wrapper;     
 
-    beforeEach(() => {
-        const mockUseEffect = jest.fn();
-        React.useEffect = mockUseEffect;
-        wrapper = shallowSetup();
-      });
+//     beforeEach(() => {
+//         const mockUseEffect = jest.fn();
+//         React.useEffect = mockUseEffect;
+//         wrapper = shallowSetup();
+//       });
     
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+//     afterEach(() => {
+//         jest.clearAllMocks();
+//     });
 
-    it('should render ', () => {
-        // wrapper = wrapper.enzymeWrapper;
+//     it('should render ', () => {
+//         // wrapper = wrapper.enzymeWrapper;
         
-        // expect(wrapper.containsMatchingElement(<button>Docker Compose Up</button>)).toBe(true);
-        // // expect(props.fetchAuthors).toHaveBeenCalled();
-        // console.log(wrapper.find('.fileList').text())
-    });
-  });
+//         // expect(wrapper.containsMatchingElement(<button>Docker Compose Up</button>)).toBe(true);
+//         // // expect(props.fetchAuthors).toHaveBeenCalled();
+//         // console.log(wrapper.find('.fileList').text())
+//     });
+//   });

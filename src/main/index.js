4bc11a8c2b4a2e846f4c2c568372636557a260ff
ilikecolddrinks <62187545+ilikecolddrinks@users.noreply.github.com>
2020-09-23 11:22:54 -0400

    pulling

--- src/main/index.js
+++ src/main/index.js
@@ -7,22 +7,32 @@ import { format as formatUrl } from 'url'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // global reference to mainWindow (necessary to prevent window from being garbage collected)
+let splash
 let mainWindow
 
 function createMainWindow() {
   const window = new BrowserWindow({
     width: 1300,
     height: 800,
+    show: false,
     webPreferences: {
       nodeIntegration: true,
       allowRendererProcessReuse: false
     },
   })
 
+  splash = new BrowserWindow({width: 810, height: 610, transparent: true, frame: false, alwaysOnTop: true})
+  splash.loadFile('../../assets/splash.png')
+
+  setTimeout(() => {
+    splash.destroy();
+    mainWindow.show();
+  },4000)
+
   if (isDevelopment) {
     // window.webContents.openDevTools()
   }
-
+  
   if (isDevelopment) {
     window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
   }

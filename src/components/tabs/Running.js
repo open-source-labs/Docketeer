784813f7410c8633089fd3ec778608594c2e8602
Richie Edwards <richie00edwards@gmail.com>
2020-10-22 16:41:26 -0400

    fixed exisiting bug on deprecated bar percentage plotting

--- src/components/tabs/Running.js
+++ src/components/tabs/Running.js
@@ -11,142 +11,144 @@ import ToggleDisplay from "../display/ToggleDisplay";
  */
 const Running = (props) => {
 
-	const [run, setRun] = useState("");
+  const [run, setRun] = useState("");
 
-	const handleClick = (e) => {
-		e.preventDefault();
-		props.runIm(run, props.runningList, helper.addRunning, props.addRunningContainers);
-	};
+  const handleClick = (e) => {
+    e.preventDefault();
+    props.runIm(run, props.runningList, helper.addRunning, props.addRunningContainers);
+  };
 
-	let renderRunningList = props.runningList.map((ele, i) => {
-		let cpuData = parseFloat(
-			ele["cpu"].substring(0, ele["cpu"].length - 1)
-		).toFixed(2);
-		let memoryData = parseFloat(
-			ele["mp"].substring(0, ele["mp"].length - 1)
-		).toFixed(2);
-		const stack = "stack";
-		const chartInfo = {
-			labels: ["CPU", "Memory"],
-			datasets: [
-				{
-					stack,
-					label: Math.random(),
-					backgroundColor: ["rgba(44, 130, 201, 1)", "rgba(19, 221, 29, 1)"],
-					borderColor: "rgba(0,0,0,0)",
-					borderWidth: 1,
-					data: [cpuData, memoryData],
-				},
-				{
-					stack,
-					label: Math.random(),
-					backgroundColor: ["rgba(155, 198, 233, 1)", "rgba(217, 252, 219, 1)"],
-					borderColor: "rgba(0,0,0,0)",
-					borderWidth: 1,
-					data: [(100 - cpuData).toFixed(2), (100 - memoryData).toFixed(2)],
-				},
-			],
-		}
+  let renderRunningList = props.runningList.map((ele, i) => {
+    let cpuData = parseFloat(
+      ele["cpu"].substring(0, ele["cpu"].length - 1)
+    ).toFixed(2);
+    let memoryData = parseFloat(
+      ele["mp"].substring(0, ele["mp"].length - 1)
+    ).toFixed(2);
+    const stack = "stack";
+    const chartInfo = {
+      labels: ["CPU", "Memory"],
+      datasets: [
+        {
+          stack,
+          label: Math.random(),
+          backgroundColor: ["rgba(44, 130, 201, 1)", "rgba(19, 221, 29, 1)"],
+          borderColor: "rgba(0,0,0,0)",
+          borderWidth: 1,
+          data: [cpuData, memoryData],
+          barPercentage: 0.4,
+        },
+        {
+          stack,
+          label: Math.random(),
+          backgroundColor: ["rgba(155, 198, 233, 1)", "rgba(217, 252, 219, 1)"],
+          borderColor: "rgba(0,0,0,0)",
+          borderWidth: 1,
+          data: [(100 - cpuData).toFixed(2), (100 - memoryData).toFixed(2)],
+          barPercentage: 0.4,
+        },
+      ],
+    };
 
-		return (
-			<div className="box box-running" key={`runningBox${i}`}>
-				<div className="box-label">
-					<span>Name: {ele["name"]}</span>
-					<span>ID: {ele["cid"]}</span>
-				</div>
-				<div className="box-info">
-					<div className="chart">
-						<div className="chart-label">
-							<div className="chart-label-container">
-								<div className="cpuBox"></div>
-								<div>
-									<span className="chart-label-text">{cpuData}%</span>
-								</div>
-							</div>
-							<div className="chart-label-container">
-								<div className="memoryBox"></div>
-								<div>
-									<span className="chart-label-text">{memoryData}%</span>
-								</div>
-							</div>
-						</div>
-						<div className="chart-info">
-							<Bar
-								data={chartInfo}
-								options={{
-									tooltips: {
-										enabled: false,
-									},
-									title: {
-										display: false,
-									},
-									legend: {
-										display: false,
-										position: "right",
-									},
+    return (
+      <div className="box box-running" key={`runningBox${i}`}>
+        <div className="box-label">
+          <span>Name: {ele["name"]}</span>
+          <span>ID: {ele["cid"]}</span>
+        </div>
+        <div className="box-info">
+          <div className="chart">
+            <div className="chart-label">
+              <div className="chart-label-container">
+                <div className="cpuBox"></div>
+                <div>
+                  <span className="chart-label-text">{cpuData}%</span>
+                </div>
+              </div>
+              <div className="chart-label-container">
+                <div className="memoryBox"></div>
+                <div>
+                  <span className="chart-label-text">{memoryData}%</span>
+                </div>
+              </div>
+            </div>
+            <div className="chart-info">
+              <Bar
+                data={chartInfo}
+                options={{
+                  tooltips: {
+                    enabled: false,
+                  },
+                  title: {
+                    display: false,
+                  },
+                  legend: {
+                    display: false,
+                    position: "right",
+                  },
 
-									scales: {
-										yAxes: [
-											{
-												gridLines: {
-													display: false,
-												},
-												ticks: {
-													display: false,
-													min: 0,
-													max: 100,
-													stepSize: 20,
-												},
-											},
-										],
-										xAxes: [
-											{
-												barPercentage: 0.4,
-												categorySpacing: 0,
-											},
-										],
-									},
-								}}
-							/>
-						</div>
-					</div>
-					<ToggleDisplay ele={ele} />
-				</div>
-				<div className="box-button box-button-running">
-					<button
-						className="stop-btn"
-						onClick={() => props.stop(ele["cid"], props.stopRunningContainer)}
-					>
-						STOP
+                  scales: {
+                    yAxes: [
+                      {
+                        gridLines: {
+                          display: false,
+                        },
+                        ticks: {
+                          display: false,
+                          min: 0,
+                          max: 100,
+                          stepSize: 20,
+                        },
+                      },
+                    ],
+                    xAxes: [
+                      {
+                        //barPercentage: 0.4,
+                        categorySpacing: 0,
+                      },
+                    ],
+                  },
+                }}
+              />
+            </div>
+          </div>
+          <ToggleDisplay ele={ele} />
+        </div>
+        <div className="box-button box-button-running">
+          <button
+            className="stop-btn"
+            onClick={() => props.stop(ele["cid"], props.stopRunningContainer)}
+          >
+            STOP
           </button>
-				</div>
-			</div>
-		);
-	});
+        </div>
+      </div>
+    );
+  });
 
-	return (
-		<div className="renderContainers">
-			<div className="header">
-				<span className="tabTitle">Running Containers</span>
-				<div className="runByButton">
-					<label>Enter Image ID or Repo</label>
-					<span>
-						<input
-							type="text"
-							value={run}
-							onChange={(e) => {
-								setRun(e.target.value);
-							}}
-						></input>
-					</span>
-					<button className="run-btn" onClick={(e) => handleClick(e)}>
-						Run
+  return (
+    <div className="renderContainers">
+      <div className="header">
+        <span className="tabTitle">Running Containers</span>
+        <div className="runByButton">
+          <label>Enter Image ID or Repo</label>
+          <span>
+            <input
+              type="text"
+              value={run}
+              onChange={(e) => {
+                setRun(e.target.value);
+              }}
+            ></input>
+          </span>
+          <button className="run-btn" onClick={(e) => handleClick(e)}>
+            Run
           </button>
-				</div>
-			</div>
-			<div className="containers">{renderRunningList}</div>
-		</div>
-	);
+        </div>
+      </div>
+      <div className="containers">{renderRunningList}</div>
+    </div>
+  );
 };
 
 export default Running;

8f524523c94ba3eb78b8972c4765c5d17b07bf10
Mitesh Patel <mit06@hotmail.com>
2020-10-22 22:16:54 +0100

    before changing state to using redux instead of hooks

--- src/components/tabs/LTMetrics.js
+++ src/components/tabs/LTMetrics.js
@@ -11,35 +11,32 @@ import { Pie, Line } from "react-chartjs-2";
  */
 const Metrics = (props) => {
 	const [activeContainers, setActiveContainers] = useState({});
+	const [axis, setAxis] = useState([]);
+	const [memory, setMemory] = useState([]);
+	const [cpu, setCpu] = useState([]);
 
-
-	
-	let memory = {
-		labels: [1],
-		datasets: []
-	};
-
-	const cpu = {
-		labels: [],
-		datasets: []
-	};
+	const bothLabels = []
+	const memoryObj = {
+		labels: bothLabels,
+		datasets: memory,
+	}
+	const cpuObj = {
+		labels: bothLabels,
+		datasets: cpu,
+	}
 
 	const formatData = () => {
-
-		console.log('active containers: ', activeContainers)
 		//if active containers is empty render the empty graphs
 		if (!Object.keys(activeContainers).length) {
-			console.log('inside conditional')
-			return;
+			setMemory([]);
+			setCpu([]);
+				return;
 		}
 		// DB QUERY LIKELY GOING HERE
-		const data = [	{name: 'dazzling_jennings', time: "1", block: "0B/0B", cid: "db06b75e6db7", cpu: "4.00%", mp: "0.18%", mul: "2.523MiB/1.945GiB", net: "50B/0B", pids: "3"}]
+		const data = [	{time:'1', name: 'amazing_morse', block: "0B/0B", cid: "db06b75e6db7", cpu: "4.00%", mp: "0.18%", mul: "2.523MiB/1.945GiB", net: "50B/0B", pids: "3"}, {name: 'amazing_morse', time: "2", block: "0B/0B", cid: "db06b75e6db7", cpu: "4.00%", mp: "2%", mul: "2.523MiB/1.945GiB", net: "50B/0B", pids: "3"}, {name: 'amazing_morse', time: "3", block: "0B/0B", cid: "db06b75e6db7", cpu: "4.00%", mp: "5.18%", mul: "2.523MiB/1.945GiB", net: "50B/0B", pids: "3"}]
 		// reset datasets to empty array
-		memory.datasets = [];
-		cpu.datasets = [];
-		
-		console.log('ContainerName: ', data)
-
+		setMemory([]);
+		setCpu([]);
 
 		// build two fundtion that will return formated object for each container to in datapoins
 		const cpuBuilder = (containerName) => {
@@ -70,30 +67,35 @@ const Metrics = (props) => {
 				cpu: cpuBuilder(container)
 			}
 		});
-
-		console.log('this is your auxObject before data.forEach: ', auxObj)
 		
-		// iterate through each row from query and buld Memory and CPU objects
+		// iterate through each row from query and buld Memory and CPU objects [{ }, {} ]
 		data.forEach((dataPoint) => {
 			const currentContainer = dataPoint.name;
-
-			// console.log('data point: ',dataPoint);
-
-			// console.log('data point Cpu: ', dataPoint.cpu);
 			auxObj[currentContainer].cpu.data.push(dataPoint.cpu.replace('%', ''))
 			auxObj[currentContainer].memory.data.push(dataPoint.mp.replace('%', ''))
-			console.log('Auxobj: ', auxObj);
+
+			bothLabels.push(Number(dataPoint.time));
+			console.log('bothLabels: ', bothLabels)
+			// add x data points to 	
+			// const axisCopy = JSON.parse(JSON.stringify(axis));
+			// axisCopy.push(dataPoint.time);
+			// console.log('axisCopy: ',axisCopy)
+			// console.log('datapoint: ',dataPoint)
+			// setAxis(axisCopy);
+			// console.log('here is axis: ', axis)
 		});
 
 
 		
 		Object.keys(auxObj).forEach(containerName => {
-			memory.datasets.push(auxObj[containerName].memory);
-			cpu.datasets.push(auxObj[containerName].cpu);
+			const memoryCopy = JSON.parse(JSON.stringify(memory))
+			memoryCopy.push(auxObj[containerName].memory);
+			setMemory(memoryCopy);
+			
+			const cpuCopy = JSON.parse(JSON.stringify(cpu))
+			cpuCopy.push(auxObj[containerName].cpu);
+			setCpu(cpuCopy);
 		});	
-
-		console.log('memory after : ', memory)
-		console.log('cpu after : ', cpu);
 	}
 
 	
@@ -109,7 +111,7 @@ const Metrics = (props) => {
 		// <input name="container-1" type="checkbox" value="Container-1"></input>
 		// <label htmlFor="container-1">Container 1</label>
 		props.stoppedList.forEach(container => {
-			result.push(<div><label htmlFor={container.name}>{container.name}</label><input name={container.name + "(- Stopped)"} type="checkbox" value={container.name}></input></div>)
+			result.push(<div><label htmlFor={container.name}>{container.name}</label><input name={container.name} type="checkbox" value={container.name}></input></div>)
 		})
 		currentList = result; 
 	}
@@ -160,7 +162,7 @@ const Metrics = (props) => {
 	
 	selectList();
 	useEffect(() => {
-		console.log('in use effect with memory: ', memory)
+		console.log('both labels before formatData is: ', bothLabels)
 		formatData();
 	}, [activeContainers])
 
@@ -179,13 +181,13 @@ const Metrics = (props) => {
 				
 				<div className="line-section">
 					<div className="lineChart">
-						<Line data={memory} options={memoryOptions} width={4000} height={2600} />
+						<Line data={memoryObj} options={memoryOptions} width={4000} height={2600} />
 					</div>
 				</div>
 
 				<div className="line-section">
 					<div className="lineChart">
-						<Line data={cpu} options={cpuOptions} width={4000} height={2600} />
+						<Line data={cpuObj} options={cpuOptions} width={4000} height={2600} />
 					</div>
 				</div>
 

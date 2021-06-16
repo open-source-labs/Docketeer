6041bbd4ef56418d3ba5c5b12711457871c3cacb
Catherine Larcheveque <79517320+clarcheveque@users.noreply.github.com>
2021-06-16 14:20:26 -0700

    set up backend routes to update contact preference, update cpu/memory thresholds, and update notification preferences for container stops in the database

--- server/controllers/configController.js
+++ server/controllers/configController.js
@@ -1,10 +1,10 @@
 /**
  * ************************************
  *
- * @module UserController
+ * @module ConfigController
  * @author Catherine Larcheveque, Lorenzo Guevara, Charles Ryu, Griffin Silver, Alex Smith
  * @date 6/14/2021
- * @description Contains middleware that creates new user in database, gets all users from database for system admin, and verifies user exists before sending back user data to login component
+ * @description Contains middleware that updates a user's contact preference, CPU threshold, memory threshold, and container stop preference in database
  *
  * ************************************
  */
@@ -37,6 +37,102 @@ configController.configureThresholds = (req, res, next) => {
       });
     });
 
+};
+
+// configure contact preference
+configController.updateContactPref = (req, res, next) => {
+  if (res.locals.error) return next();
+
+  const { contact_pref, _id } = req.body;
+  
+  const inputPref = 'UPDATE users SET contact_pref = $1 WHERE _id = $2 RETURNING *;';
+  const prefDetails = [contact_pref, _id];
+
+  db.query(inputPref, prefDetails)
+    .then((data) => {
+      res.locals.pref = data.rows[0];
+      console.log(res.locals.pref);
+      console.log('contact preference updated');
+      return next();
+    })
+    .catch((err) => {
+      return next({
+        log: `Error in configController updateContactPref: ${err}`,
+        message: { err: 'An error occured updating contact preferences in database. See configController.updateContactPref.' },
+      });
+    });
+};
+
+// configure CPU threshold
+configController.updateCPUThreshold = (req, res, next) => {
+  if (res.locals.error) return next();
+
+  const { cpu_threshold, _id } = req.body;
+  
+  const inputCPU = 'UPDATE users SET cpu_threshold = $1 WHERE _id = $2 RETURNING *;';
+  const CPUDetails = [cpu_threshold, _id];
+
+  db.query(inputCPU, CPUDetails)
+    .then((data) => {
+      res.locals.pref = data.rows[0];
+      console.log(res.locals.pref);
+      console.log('CPU threshold updated');
+      return next();
+    })
+    .catch((err) => {
+      return next({
+        log: `Error in configController updateCPUThreshold: ${err}`,
+        message: { err: 'An error occured updating CPU threshold in database. See configController.updateCPUThreshold.' },
+      });
+    });
+};
+
+// configure memory threshold
+configController.updateMemThreshold = (req, res, next) => {
+  if (res.locals.error) return next();
+
+  const { mem_threshold, _id } = req.body;
+  
+  const inputMem = 'UPDATE users SET mem_threshold = $1 WHERE _id = $2 RETURNING *;';
+  const memDetails = [mem_threshold, _id];
+
+  db.query(inputMem, memDetails)
+    .then((data) => {
+      res.locals.pref = data.rows[0];
+      console.log(res.locals.pref);
+      console.log('memory threshold updated');
+      return next();
+    })
+    .catch((err) => {
+      return next({
+        log: `Error in configController updateMemThreshold: ${err}`,
+        message: { err: 'An error occured updating memory threshold in database. See configController.updateMemThreshold.' },
+      });
+    });
+};
+
+// configure preference to receive notification when a container stops running
+configController.updateStopPref = (req, res, next) => {
+  if (res.locals.error) return next();
+
+  const { container_stops, _id } = req.body;
+  
+  const inputStopPref = 'UPDATE users SET container_stops = $1 WHERE _id = $2 RETURNING *;';
+  const stopPrefDetails = [container_stops, _id];
+
+  db.query(inputStopPref, stopPrefDetails)
+    .then((data) => {
+      res.locals.pref = data.rows[0];
+      console.log(res.locals.pref);
+      console.log('container stops updated');
+      return next();
+    })
+    .catch((err) => {
+      return next({
+        log: `Error in configController updateStopPref: ${err}`,
+        message: { err: 'An error occured updating container stop preference in database. See configController.updateStopPref.' },
+      });
+    });
 };
 
 module.exports = configController;

3ec3d3c7708e77c56248c29d3e28dcb2837397f9
Eric Lee <emlee54@gmail.com>
2021-10-04 10:51:29 -0700

    Small fixes: removed console.logs

--- src/main/slack/cpuNotification.js
+++ src/main/slack/cpuNotification.js
@@ -1,29 +1,20 @@
-// got slack messaging working
-
 const request = require('request');
-
-// import dotenv
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const URL = process.env.URL;
 
-const cpuNotification = async function(){
+const cpuNotification = async function() {
   try {
-    console.log('here');
     const payload = {"text": "The CPU threshold has been met or exceeded"};
-    console.log('here2');
-    (await request({
+    await request({
       url: URL,
       method: 'POST',
       body: payload,
       json: true
-    }));
-  } catch(e){
-    console.log('this is our error', e);
+    });
+  } catch(err){
+    console.log('cpuNotification ERR: ', err);
   }
 };
 
-
 export default cpuNotification;
-
--- src/main/slack/memoryNotification.js
+++ src/main/slack/memoryNotification.js
@@ -1,24 +1,19 @@
 const request = require('request');
-
-// import dotenv
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const URL = process.env.URL;
 
-const memoryNotification = async function(){
+const memoryNotification = async function() {
   try {
-    console.log('here');
     const payload = {"text": "The Memory threshold has been met or exceeded"};
-    console.log('here2')
-    (await request({
+    await request({
       url: URL,
       method: 'POST',
       body: payload,
       json: true
-    }));
-  } catch(e){
-    console.log('this is our error', e);
+    });
+  } catch(err){
+    console.log('memoryNotification ERR: ', err);
   }
 };
 

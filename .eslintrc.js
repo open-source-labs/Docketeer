70ca58dbfae1a2f3478d0ba1941a2976a0eb7fa6
Eric Lee <emlee54@gmail.com>
2021-10-04 15:58:43 -0700

    Updated eslint files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,21 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
-    'node': true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:react/recommended'
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 12,
-    'sourceType': 'module'
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  'plugins': [
-    'react'
-  ],
-  'rules': {
-    'indent': ['warn', 2],
+  plugins: ['react'],
+  rules: {
+    indent: ['warn', 2],
     'no-unused-vars': ['off', { vars: 'local' }],
     'prefer-const': 'warn',
     quotes: ['warn', 'single'],
@@ -30,20 +25,25 @@ module.exports = {
     'no-restricted-syntax': [
       'error',
       {
-        'selector': 'CallExpression[callee.object.name=\'console\'][callee.property.name!=/^(log|warn|error|info|trace)$/]',
-        'message': 'Unexpected property on console object was called'
-      }
-    ],
-    'spaced-comment': ['error', 'always', {
-      'line': {
-        'markers': ['/'],
-        'exceptions': ['-', '+']
+        selector:
+          "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+        message: 'Unexpected property on console object was called',
       },
-      'block': {
-        'markers': ['!'],
-        'exceptions': ['*'],
-        'balanced': true
-      }
-    }]
-  }
+    ],
+    'spaced-comment': [
+      'error',
+      'always',
+      {
+        line: {
+          markers: ['/'],
+          exceptions: ['-', '+'],
+        },
+        block: {
+          markers: ['!'],
+          exceptions: ['*'],
+          balanced: true,
+        },
+      },
+    ],
+  },
 };

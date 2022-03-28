425c3eca6164c03d14dc16405534e579b0c33a51
Austin Andrews <austinandrews@berkeley.edu>
2022-03-28 11:55:53 -0700

    another logs function commit

--- src/components/helper/scratchCommandScratch.js
+++ src/components/helper/scratchCommandScratch.js
@@ -12,69 +12,68 @@ options obj template:
 */
 
 const testOptionsObject = {
-  containerId: '1cb7e06fb524',
-  since: '',
-  tail: '100'
+    containerId: '1cb7e06fb524',
+    since: '',
+    tail: '100'
 };
 
 function buildLogsCommand(obj) {
-  let commandString = 'docker logs --timestamps ';
-  if (obj.since) {
-    console.log(obj.since);
-    commandString += `--since ${obj.since} `;
-  }
+    let commandString = 'docker logs --timestamps ';
+    if (obj.since) {
+        console.log(obj.since);
+        commandString += `--since ${obj.since} `;
+    }
 
-  obj.tail ? commandString += `--tail ${obj.tail} ` : commandString += `--tail 50 `
+    obj.tail ? commandString += `--tail ${obj.tail} ` : commandString += `--tail 50 `
 
-  return commandString += `${obj.containerId}`;
+    return commandString += `${obj.containerId}`;
 }
 
+//makeArrayOfObjects transforms input string (where string is a batch of logs) to an array of objects: [{logmsg:..., timestamp:...}, {logmsg:..., timestamp:...}, ...]
 function makeArrayOfObjects(string) {
-  const arrayOfObjects = string.trim().split('\n').map((element) => {
-    let obj = {};
-    const timeStampLogArray = element.split(' ');
-    if (timeStampLogArray[0].endsWith('Z')) {
-      const timeStamp = timeStampLogArray.shift();
-      const logMsg = timeStampLogArray.join(' ');
-      obj.timeStamp = timeStamp;
-      obj.logMsg = logMsg.trim();
-    }
-    else {
-      obj.logMsg = timeStampLogArray.join(' ').trim();
-      obj.timeStamp = '----';
-    }
-    return obj;
-  });
-  return arrayOfObjects;
+    const arrayOfObjects = string.trim().split('\n').map((element) => {
+        let obj = {};
+        const timeStampLogArray = element.split(' ');
+        if (timeStampLogArray[0].endsWith('Z')) {
+            const timeStamp = timeStampLogArray.shift();
+            const logMsg = timeStampLogArray.join(' ');
+            obj.timeStamp = timeStamp;
+            obj.logMsg = logMsg.trim();
+        }
+        else {
+            obj.logMsg = timeStampLogArray.join(' ').trim();
+            obj.timeStamp = '----';
+        }
+        return obj;
+    });
+    return arrayOfObjects;
 };
 
 const inputCommandString = buildLogsCommand(testOptionsObject);
 
 exec(inputCommandString, (error, stdout, stderr) => {
-  // console.log(`stdout: ${stdout}`);
-  //error handling
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
+    // console.log(`stdout: ${stdout}`);
+    //error handling
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
 
-  console.log(makeArrayOfObjects(stdout))
-  //transforming string (where string is a batch of logs) to an array of objects: [{logmsg:..., timestamp:...}, {logmsg:..., timestamp:...}, ...]
-  //this is done for both stdout stream and stderr stream
+    //   console.log(makeArrayOfObjects(stdout))
 
-  if (!stderr && !stdout) {
-    return 'No logs to display';
-  }
-  else if (stdout && !stderr) {
-    return makeArrayOfObjects(stdout);
-  }
-  else if (stderr && !stdout) {
-    return makeArrayOfObjects(stderr);
-  }
-  else {
-    return [makeArrayOfObjects(stdout), makeArrayOfObjects(stderr)]
-  }
 
+    if (!stderr && !stdout) {
+        return 'No logs to display';
+    }
+    else if (stdout && !stderr) {
+        return makeArrayOfObjects(stdout);
+    }
+    else if (stderr && !stdout) {
+        return makeArrayOfObjects(stderr);
+    }
+    else {
+        return [makeArrayOfObjects(stdout), makeArrayOfObjects(stderr)]
+    };
 });
 
 

dd800a98880b91cc63dc03f4789c1146c3f0a3f8
Austin Andrews <austinandrews@berkeley.edu>
2022-03-28 17:18:50 -0700

    stable version of logs algo

--- src/components/helper/scratchCommandScratch.js
+++ src/components/helper/scratchCommandScratch.js
@@ -1,20 +1,34 @@
 import { exec } from 'child_process';
 
 /*
+This file expects an options object to be passed to it.
+
 options obj template:
 {
     containerId: string,
-    details: boolean,
-    timestamps: boolean,
     since: string,
     tail: string
 }
+
+The options object is passed to buildLogsCommand. buildLogsCommand creates a Docker command, of the form docker logs [options] containerid.
+
+This command is then passed to exec. Exec generates an array objects, where each object is of the form:
+
+{
+    timestamp: string,
+    logMsg: string
+}
+
+Each object represents a single docker log line.
+
+If there are messages piped to *both* stdout and stderr, exec returns an array of arrays, where the first array is the array of stdout log objects and the second array is the array of stderr log objects
 */
+
 
 const testOptionsObject = {
     containerId: '1cb7e06fb524',
     since: '',
-    tail: '100'
+    tail: ''
 };
 
 function buildLogsCommand(obj) {
@@ -23,13 +37,12 @@ function buildLogsCommand(obj) {
         console.log(obj.since);
         commandString += `--since ${obj.since} `;
     }
-
     obj.tail ? commandString += `--tail ${obj.tail} ` : commandString += `--tail 50 `
-
     return commandString += `${obj.containerId}`;
 }
 
 //makeArrayOfObjects transforms input string (where string is a batch of logs) to an array of objects: [{logmsg:..., timestamp:...}, {logmsg:..., timestamp:...}, ...]
+
 function makeArrayOfObjects(string) {
     const arrayOfObjects = string.trim().split('\n').map((element) => {
         let obj = {};
@@ -52,15 +65,10 @@ function makeArrayOfObjects(string) {
 const inputCommandString = buildLogsCommand(testOptionsObject);
 
 exec(inputCommandString, (error, stdout, stderr) => {
-    // console.log(`stdout: ${stdout}`);
-    //error handling
     if (error) {
         console.error(`exec error: ${error}`);
         return;
     }
-
-    //   console.log(makeArrayOfObjects(stdout))
-
 
     if (!stderr && !stdout) {
         return 'No logs to display';

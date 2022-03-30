ae5bebdb4cad46f88f285c163418724b3641042e
lunaf-github <lunaf91@gmail.com>
2022-03-30 11:46:35 -0700

    Add process log container

--- src/components/tabs/ProcessLogs.js
+++ src/components/tabs/ProcessLogs.js
@@ -1,82 +1,62 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from "react";
+import React from 'react';
+import ToggleDisplay from '../display/ToggleDisplay';
+import { HashRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import ProcessLogsCard from '../display/ProcessLogsCard';
+
 
 /**
- * Render Volume History
- * May need to be refactored to auto refresh
- *
+ * Display all running and stopped containers. Each box can be selected to view process log tables.
+ * 
  * @param {*} props
  */
-const processLogs = (props) => {
-  //   const [volumeName, setVolumeName] = useState("");
-  //   const [volumeList, setVolumeList] = useState("");
 
-  // Container details
-  //const containerDetails = (container, i) => {
-  // unique key error here, fix required
-  //     return (
-  //       <div
-  //         className="volume-container-details"
-  //         key={`vol.${container.Names}-${i}`}
-  //       >
-  //         <strong>Container Name: </strong>
-  //         {container["Names"]}
-  //         <ul>
-  //           <li>
-  //             <strong>Status: </strong>
-  //             {container["State"]}
-  //           </li>
-  //           <li>
-  //             <strong>Running For: </strong>
-  //             {container["Status"]}
-  //           </li>
-  //         </ul>
-  //       </div>
-  //     );
-  //   };
 
-  // Creates the card components of Name and container details
-  //   const renderVolumeHistory = (volumeProps) =>
-  //     volumeProps.map((ele, i) => {
-  //       const details = [];
+const ProcessLogs = (props) => {
 
-  //       ele.containers.length
-  //         ? ele.containers.forEach((el) => details.push(containerDetails(el, i)))
-  //         : details.push(
-  //             <div className="volume-container-details">
-  //               No container found in this volume
-  //             </div>
-  //           );
 
-  //       return (
-  //         <div className="box" key={`volume${i}`}>
-  //           <div className="box-label">
-  //             <h3>{ele.vol_name}</h3>
-  //           </div>
-  //           {details}
-  //         </div>
-  //       );
-  //     });
 
-  //   // Initializes the volume history tab to be the list of volumes
-  //   let renderList = renderVolumeHistory(props.volumeContainersList);
+  const renderRunningList = [];
+  props.runningList.map((container, index) => {
+    renderRunningList.push(
+      <Link to={`/LogTable/${container.ID}`}> 
+      {/* <Link to='/logTable'> */}
+        <ProcessLogsCard index={index} container={container} key={container.ID} status="Running" />
+      </Link>
+    );
+  });
 
-  //   // Search bar: Finds volume name and renders an individual card
-  //   const handleClick = (e) => {
-  //     e.preventDefault();
-  //     const result = volumeList.filter((vol) => vol.Name.includes(volumeName));
 
-  //     setVolumeList(result);
-  //     renderList = renderVolumeHistory(volumeList);
-  //   };
+  const renderStoppedList = [];
+  props.stoppedList.map((container, index) => {
+    renderStoppedList.push(<ProcessLogsCard index={index} container={container} status="Stopped" />);
+  });
 
   return (
     <div className="renderContainers">
-      <div className="header">
-        <h1 className="tabTitle">Process Logs</h1>
+      <div className='header'>
+        <h1 className='tabTitle'>Process Logs</h1>
       </div>
+
+ 
+      <h3>
+          Running Containers: {props.runningList.length}
+      </h3>
+
+      <div className="containers">
+        {renderRunningList}
+      </div>
+
+      <h3>
+        Stopped Containers: {props.stoppedList.length}
+      </h3>
+
+      <div className="containers">
+        {renderStoppedList}
+      </div>
+
     </div>
   );
 };
 
-export default processLogs;
+export default ProcessLogs;

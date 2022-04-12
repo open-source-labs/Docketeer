f19e6890416d66f6871a849f550060cf8e64abe4
lunaf-github <lunaf91@gmail.com>
2022-04-12 14:16:17 -0700

    Fix eslint error within try catch

--- src/reducers/userListReducer.js
+++ src/reducers/userListReducer.js
@@ -7,29 +7,30 @@ const initialState = {
 export default function (state = initialState, action){
   switch(action.type) {
 
-    // Change isLoggedIn state variable depending on previous value
-    case types.UPDATE_USER_LIST:
-      return { 
-        userList: action.payload,  
-      };
+  // Change isLoggedIn state variable depending on previous value
+  case types.UPDATE_USER_LIST:
+    return { 
+      userList: action.payload,  
+    };
 
-    case types.UPDATE_USER_ROLE: 
-      const { _id, role } = action.payload;
-      const newUserList = [...state.userList];
-      for (let i = 0; i < newUserList.length; i++){
-        if (newUserList[i]._id === _id){
-          newUserList[i].role = role;
-          break;
-        }
+  case types.UPDATE_USER_ROLE: {
+    const { _id, role } = action.payload;
+    const newUserList = [...state.userList];
+    for (let i = 0; i < newUserList.length; i++){
+      if (newUserList[i]._id === _id){
+        newUserList[i].role = role;
+        break;
       }
-      return {
-        ...state,
-        userList: newUserList,
-      };
-
-    default: 
-      return {
-        ...state
-      };
+    }
+    return {
+      ...state,
+      userList: newUserList,
+    };
   }
-};
+  default: 
+    return {
+      ...state
+    };
+  }
+
+}

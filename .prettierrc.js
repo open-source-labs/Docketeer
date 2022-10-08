ab2f68cde71a9c03e6d3522ed600888c822fdafa
errc-lee <77417178+errc-lee@users.noreply.github.com>
2022-10-08 11:32:31 -0700

    fix: lint

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   printWidth: 80,
   tabWidth: 2,
   singleQuote: true,
-  trailingComma: 'none',
+  trailingComma: "es5",
   bracketSpacing: true,
   semi: true,
   useTabs: false,

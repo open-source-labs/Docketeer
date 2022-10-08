c4a52a5e6c53f190b76e54375a75de3ac48813ec
Matthew Dias <matthewdias13@gmail.com>
2022-10-08 14:54:04 -0700

    Additional minor edits

---
+++ src/components/helper/psqlQuery.js
@@ -0,0 +1,15 @@
+//!! PROBLEM: Causing procces undefined error
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   host: 'localhost', 
+//   user: 'postgres',
+//   password: 'postgres',
+//   database: 'docketeer-db',
+//   port: 5433,
+// });
+
+// export default async (text, params, callback) => {
+//   const rows = await pool.query(text, params, callback);
+//   return rows;
+// };

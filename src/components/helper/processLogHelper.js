3cae50c85f7c03e7afeca64b1354ee766fb22f15
Abigail Gerig <abigailgerig@Abigails-MacBook-Pro.local>
2022-10-24 12:25:13 -0500

    Refactored makeArrayOfObjects to remove unnecessary text
    Co-authored-by: Christian Looff <106610236+cmlooff@users.noreply.github.com>
    Co-authored-by: Ruli Warner-Rosen <108160097+Ruliwr@users.noreply.github.com>
    Co-authored-by: Trine Medina <106996307+TrineMedina@users.noreply.github.com>
    Co-authored-by: Matt Dias <105461989+Schmang13@users.noreply.github.com>

--- src/components/helper/processLogHelper.js
+++ src/components/helper/processLogHelper.js
@@ -41,21 +41,26 @@ export const makeArrayOfObjects = (string, containerName) => {
     .split('\n')
     .map((element) => {
       const obj = {};
-      const timeStampLogArray = element.split(' ');
-      if (timeStampLogArray[0].endsWith('Z')) {
-        const timeStamp = timeStampLogArray.shift();
-        const logMsg = timeStampLogArray.join(' ');
+      const logArray = element.split(' ');
+      // extract timestamp
+      if (logArray[0].endsWith('Z')) {
+        const timeStamp = logArray.shift();
+        // parse GMT string to be readable local date and time
         obj.timeStamp = new Date(Date.parse(timeStamp)).toLocaleString();
-        obj.logMsg = logMsg.trim();
-      } else {
-        if (timeStampLogArray.join(' ').trim() === '') {
-          obj.timeStamp = '';
-          obj.logMsg = '';
-        } else {
-          obj.logMsg = timeStampLogArray.join(' ').trim();
-          obj.timeStamp = '----';
-        }
       }
+      // parse remaining array to create readable message
+      let logMsg = logArray.join(' ');
+      // messages with duplicate time&date have form: '<Time/Date> [num/notice] actual msg'
+      const closingIndex = logMsg.indexOf(']');
+      if (closingIndex >= 0) {
+        logMsg = logMsg.slice(closingIndex + 1).trim();
+      }
+      // after removing [num/notice], some logs also have 'LOG:' before the actual msg
+      if (logMsg.slice(0, 4) === 'LOG:') {
+        console.log('made it in if');
+        logMsg = logMsg.slice(4);
+      }
+      obj.logMsg = logMsg.trim();
       obj.containerName = containerName;
       return obj;
     });

b1144ee4c540eebaf0be6d68938c31b33650826f
cmloof <cmlooff@gmail.com>
2022-10-24 15:26:00 -0400

    Install React-Testing-Library to replace enzyme.
    Still need work to update current test files
    
    Co-authored-by: Abigail Gerig <94636591+4estgirl@users.noreply.github.com>
    Co-authored-by: Ruli Warner-Rosen <108160097+Ruliwr@users.noreply.github.com>
    Co-authored-by: Trine Medina <106996307+TrineMedina@users.noreply.github.com>
    Co-authored-by: Matt Dias <105461989+Schmang13@users.noreply.github.com>

--- __tests__/test_file_isolation/ServerRoutes.test.js
+++ __tests__/test_file_isolation/ServerRoutes.test.js
@@ -1,107 +1,98 @@
 // Need to fill in tests for Routes
 
-const request = require('supertest');
-import server from '../../server/server.js';
+const supertest = require('supertest');
+const server = require('../../server/app');
 
-// describe('Test Route /test', () => {
-//   describe('GET request on the test route', () => {
-//     // Test route to receive a status of 200 and a json object of success:true
-//     test('Should respond with a 200 status code', () => {
-//       // request method from supertest
-//       request(server)
-//         .get('/test')
-//         .expect('Content-Type', /json/)
-//         .expect(200)
-//         .end((err, res) => {
-//           if (err) throw err;
-//         });
-//     });
-//   });
+const request = supertest(server);
 
-//   describe('POST request on the test route', () => {
-//     // Test route to receive a status of 200 and a json object of success:true
-//     test('Should respond with a 200 status code', () => {
-//       // request method from supertest
-//       request(server)
-//         .post('/test')
-//         .expect('Content-Type', /json/)
-//         .expect(200)
-//         .end((err, res) => {
-//           if (err) throw err;
-//         });
-//     });
-//   });
-// });
+describe('Test Route /test', () => {
+  describe('GET request on the test route', () => {
+    // Test route to receive a status of 200 and a json object of success:true
+    test('Should respond with a 200 status code', async () => {
+      //* Debugging
+      // expect(2 + 2).toBe(4);
 
-describe('Settings Route /settings', () => {
-  // GET request for default route
-  describe('GET request /settings/', () => {
-    test('Expect status code 200 and JSON object', () => {
-      // request(server)
-      //   .get('/settings/')
-      //   .expect(200)
-      //   .expect('Content/Type', /json/);
-
-      const response = request(server).get('/settings/');
-
-      // response.
+      const response = await request.get('/test');
+      expect(response.status).toBe(200);
+      expect(response.body.success).toBe(true);
     });
   });
 
-  // describe('POST request /settings/insert', () => {
-  //   test('Expect status code 200 and JSON object', () => {
-  //     request(server)
-  //       .post('/settings/insert')
-  //       .expect(200)
-  //       .expect('Content/Type', /json/);
-  //   });
-  // });
+  describe('POST request on the test route', () => {
+    // Test route to receive a status of 200 and a json object of success:true
+    test('Should respond with a 200 status code', async () => {
+      // * Debugging
+      // expect(2 + 2).toBe(4);
 
-  // describe('POST request /settings/delete', () => {
-  //   test('Expect status code 200 and JSON object', () => {
-  //     request(server)
-  //       .post('/settings/delete')
-  //       .expect(200)
-  //       .expect('Content/Type', /json/);
-  //   });
-  // });
-
-  // describe('POST request /settings/phone', () => {
-  //   test('Expect status code 200 and JSON object', () => {
-  //     request(server)
-  //       .post('/settings/phone')
-  //       .expect(200)
-  //       .expect('Content/Type', /json/);
-  //   });
-  // });
-
-  // describe('POST request /settings/notification', () => {
-  //   test('Expect status code 200 and JSON object', () => {
-  //     request(server)
-  //       .post('/settings/notification')
-  //       .expect(200)
-  //       .expect('Content/Type', /json/);
-  //   });
-  // });
-
-  // describe('POST request /settings/monitoring', () => {
-  //   test('Expect status code 200 and JSON object', () => {
-  //     request(server)
-  //       .post('/settings/monitoring')
-  //       .expect(200)
-  //       .expect('Content/Type', /json/);
-  //   });
-  // });
-
-  // describe('POST request /settings/gitLinks', () => {
-  //   test('Expect status code 200 and JSON object', () => {
-  //     request(server)
-  //       .post('/settings/gitLinks')
-  //       .expect(200)
-  //       .expect('Content/Type', /json/);
-  //   });
-  // });
+      const response = await request.get('/test');
+      expect(response.status).toBe(200);
+      expect(response.body.success).toBe(true);
+    });
+  });
 });
+
+// describe('Settings Route /settings', () => {
+//   // GET request for default route
+//   // describe('GET request /settings/', () => {
+//   //   test('Expect status code 200 and JSON object', async (done) => {
+//   //     // return request.get('/settings/').then((response) => {
+//   //     //   expect(response.statusCode).toBe(200);
+//   //     //   done();
+//   //     // });
+//   //     const response = await request.get('/settings');
+//   //     expect(response.status).toBe(200);
+//   //     done();
+//   //   });
+//   // });
+//   // describe('POST request /settings/insert', () => {
+//   //   test('Expect status code 200 and JSON object', () => {
+//   //     request(server)
+//   //       .post('/settings/insert')
+//   //       .expect(200)
+//   //       .expect('Content/Type', /json/);
+//   //   });
+//   // });
+//   // describe('POST request /settings/delete', () => {
+//   //   test('Expect status code 200 and JSON object', () => {
+//   //     request(server)
+//   //       .post('/settings/delete')
+//   //       .expect(200)
+//   //       .expect('Content/Type', /json/);
+//   //   });
+//   // });
+//   // describe('POST request /settings/phone', () => {
+//   //   test('Expect status code 200 and JSON object', () => {
+//   //     request(server)
+//   //       .post('/settings/phone')
+//   //       .expect(200)
+//   //       .expect('Content/Type', /json/);
+//   //   });
+//   // });
+//   // describe('POST request /settings/notification', () => {
+//   //   test('Expect status code 200 and JSON object', () => {
+//   //     request(server)
+//   //       .post('/settings/notification')
+//   //       .expect(200)
+//   //       .expect('Content/Type', /json/);
+//   //   });
+//   // });
+//   // describe('POST request /settings/monitoring', () => {
+//   //   test('Expect status code 200 and JSON object', () => {
+//   //     request(server)
+//   //       .post('/settings/monitoring')
+//   //       .expect(200)
+//   //       .expect('Content/Type', /json/);
+//   //   });
+//   // });
+//   // describe('POST request /settings/gitLinks', () => {
+//   //   test('Expect status code 200 and JSON object', () => {
+//   //     request(server)
+//   //       .post('/settings/gitLinks')
+//   //       .expect(200)
+//   //       .expect('Content/Type', /json/);
+//   //   });
+//   // });
+// });
 
 // describe('Initiate Metric Database Route /init', () => {
 //   // GET request for default route

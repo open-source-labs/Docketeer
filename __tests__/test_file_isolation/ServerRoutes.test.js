d7fbf3b935970da8ddc517a89471fccd884f6d08
Matthew Dias <matthewdias13@gmail.com>
2022-10-26 22:31:59 -0700

    Merge conflict 58.

--- __tests__/test_file_isolation/ServerRoutes.test.js
+++ __tests__/test_file_isolation/ServerRoutes.test.js
@@ -37,110 +37,114 @@ describe('Settings Route /settings', () => {
   // GET request for default route
   describe('GET request /settings/', () => {
     test('Expect status code 200 and JSON object', () => {
-      request(server)
-        .post('/settings/')
-        .expect(200)
-        .expect('Content/Type', /json/);
+      // request(server)
+      //   .get('/settings/')
+      //   .expect(200)
+      //   .expect('Content/Type', /json/);
+
+      const response = request(server).get('/settings/');
+
+      // response.
     });
   });
 
-  describe('POST request /settings/insert', () => {
-    test('Expect status code 200 and JSON object', () => {
-      request(server)
-        .post('/settings/insert')
-        .expect(200)
-        .expect('Content/Type', /json/);
-    });
-  });
+  // describe('POST request /settings/insert', () => {
+  //   test('Expect status code 200 and JSON object', () => {
+  //     request(server)
+  //       .post('/settings/insert')
+  //       .expect(200)
+  //       .expect('Content/Type', /json/);
+  //   });
+  // });
 
-  describe('POST request /settings/delete', () => {
-    test('Expect status code 200 and JSON object', () => {
-      request(server)
-        .post('/settings/delete')
-        .expect(200)
-        .expect('Content/Type', /json/);
-    });
-  });
+  // describe('POST request /settings/delete', () => {
+  //   test('Expect status code 200 and JSON object', () => {
+  //     request(server)
+  //       .post('/settings/delete')
+  //       .expect(200)
+  //       .expect('Content/Type', /json/);
+  //   });
+  // });
 
-  describe('POST request /settings/phone', () => {
-    test('Expect status code 200 and JSON object', () => {
-      request(server)
-        .post('/settings/phone')
-        .expect(200)
-        .expect('Content/Type', /json/);
-    });
-  });
+  // describe('POST request /settings/phone', () => {
+  //   test('Expect status code 200 and JSON object', () => {
+  //     request(server)
+  //       .post('/settings/phone')
+  //       .expect(200)
+  //       .expect('Content/Type', /json/);
+  //   });
+  // });
 
-  describe('POST request /settings/notification', () => {
-    test('Expect status code 200 and JSON object', () => {
-      request(server)
-        .post('/settings/notification')
-        .expect(200)
-        .expect('Content/Type', /json/);
-    });
-  });
+  // describe('POST request /settings/notification', () => {
+  //   test('Expect status code 200 and JSON object', () => {
+  //     request(server)
+  //       .post('/settings/notification')
+  //       .expect(200)
+  //       .expect('Content/Type', /json/);
+  //   });
+  // });
 
-  describe('POST request /settings/monitoring', () => {
-    test('Expect status code 200 and JSON object', () => {
-      request(server)
-        .post('/settings/monitoring')
-        .expect(200)
-        .expect('Content/Type', /json/);
-    });
-  });
+  // describe('POST request /settings/monitoring', () => {
+  //   test('Expect status code 200 and JSON object', () => {
+  //     request(server)
+  //       .post('/settings/monitoring')
+  //       .expect(200)
+  //       .expect('Content/Type', /json/);
+  //   });
+  // });
 
-  describe('POST request /settings/gitLinks', () => {
-    test('Expect status code 200 and JSON object', () => {
-      request(server)
-        .post('/settings/gitLinks')
-        .expect(200)
-        .expect('Content/Type', /json/);
-    });
-  });
+  // describe('POST request /settings/gitLinks', () => {
+  //   test('Expect status code 200 and JSON object', () => {
+  //     request(server)
+  //       .post('/settings/gitLinks')
+  //       .expect(200)
+  //       .expect('Content/Type', /json/);
+  //   });
+  // });
 });
 
-describe('Initiate Metric Database Route /init', () => {
-  // GET request for default route
-  describe('GET request /init/', () => {
-    test('Expect status code 200 and JSON object', () => {
-      request(server).post('/init/').expect(200).expect('Content/Type', /json/);
-    });
-  });
+// describe('Initiate Metric Database Route /init', () => {
+//   // GET request for default route
+//   describe('GET request /init/', () => {
+//     test('Expect status code 200 and JSON object', () => {
+//       request(server).post('/init/').expect(200).expect('Content/Type', /json/);
+//     });
+//   });
 
-  // ! Expect this to fail bc of no json return
-  describe('POST request /init/timezone', () => {
-    test('Expect status code 200 and JSON object', () => {
-      request(server)
-        .post('/init/timezone')
-        .expect(200)
-        .expect('Content/Type', /json/);
-    });
-  });
+//   // ! Expect this to fail bc of no json return
+//   describe('POST request /init/timezone', () => {
+//     test('Expect status code 200 and JSON object', () => {
+//       request(server)
+//         .post('/init/timezone')
+//         .expect(200)
+//         .expect('Content/Type', /json/);
+//     });
+//   });
 
-  describe('POST request /init/github', () => {
-    test('Expect status code 200 and JSON object', () => {
-      request(server)
-        .post('/init/github')
-        .expect(200)
-        .expect('Content/Type', /json/);
-    });
-  });
+//   describe('POST request /init/github', () => {
+//     test('Expect status code 200 and JSON object', () => {
+//       request(server)
+//         .post('/init/github')
+//         .expect(200)
+//         .expect('Content/Type', /json/);
+//     });
+//   });
 
-  describe('POST request /init/addMetrics', () => {
-    test('Expect status code 200 and JSON object', () => {
-      request(server)
-        .post('/init/addMetrics')
-        .expect(200)
-        .expect('Content/Type', /json/);
-    });
-  });
+//   describe('POST request /init/addMetrics', () => {
+//     test('Expect status code 200 and JSON object', () => {
+//       request(server)
+//         .post('/init/addMetrics')
+//         .expect(200)
+//         .expect('Content/Type', /json/);
+//     });
+//   });
 
-  describe('POST request /init/getMetrics', () => {
-    test('Expect status code 200 and JSON object', () => {
-      request(server)
-        .post('/init/getMetrics')
-        .expect(200)
-        .expect('Content/Type', /json/);
-    });
-  });
-});
+//   describe('POST request /init/getMetrics', () => {
+//     test('Expect status code 200 and JSON object', () => {
+//       request(server)
+//         .post('/init/getMetrics')
+//         .expect(200)
+//         .expect('Content/Type', /json/);
+//     });
+//   });
+// });
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 <<<<<<< HEAD
 // const path = require('path');
@@ -5,58 +6,13 @@ const cors = require('cors');
 =======
 const path = require('path');
 const cors = require('cors');
+=======
+const app = require('./app');
+>>>>>>> c4ce03b (Test seperating server file into app and server files)
 const colors = require('colors');
 >>>>>>> ceb41ea (@types/pg dependency)
 
-// Routers
-const signupRouter = require('./routes/signupRouter');
-const loginRouter = require('./routes/loginRouter');
-const adminRouter = require('./routes/adminRouter');
-const accountRouter = require('./routes/accountRouter');
-const apiRouter = require('./routes/apiRouter');
-const dbRouter = require('./routes/dbRouter');
-const initRouter = require('./routes/initRouter');
-const logoutRouter = require('./routes/logoutRouter');
-const settingsRouter = require('./routes/settingsRouter');
-
-const app = express();
 const PORT = 3000;
-
-app.use(express.json()); // parses the request body
-app.use(express.urlencoded({ extended: true })); // parses urlencoded payloads
-app.use(cors()); // enables ALL cors requests
-
-app.use('/test', (req, res) => {
-  res.status(200).json({
-    success: true
-  });
-});
-
-app.use('/settings', settingsRouter);
-app.use('/init', initRouter);
-app.use('/signup', signupRouter);
-app.use('/login', loginRouter);
-app.use('/admin', adminRouter);
-app.use('/account', accountRouter);
-app.use('/api', apiRouter);
-app.use('/db', dbRouter);
-app.use('/logout', logoutRouter);
-
-// Unknown Endpoint Error Handler
-app.use('/', (req, res) => {
-  return res.status(404).json('404 Not Found');
-});
-
-// Global Error Handler
-app.get('/', (req, res, next, err) => {
-  const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 500,
-    message: { err: 'An error occured' }
-  };
-  const errorObj = Object.assign(defaultErr, err);
-  return res.status(errorObj.status).json(errorObj.message);
-});
 
 // Open up server on PORT
 app.listen(PORT, () => {
@@ -66,5 +22,3 @@ app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`.green.inverse);
 >>>>>>> ceb41ea (@types/pg dependency)
 });
-
-module.exports = app;

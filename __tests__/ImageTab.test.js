31ca9a24818468c167ea174fe257c08d6b6d2bbc
Drew Manley <andrewbmanley@gmail.com>
2022-11-25 15:07:29 -0600

    added UsersTab.test  to __tests__ and worked on ImageTab.test
    Co-authored-by: Sarah Moosa <74516787+Sbethm@users.noreply.github.com>
    Co-authored-by: Tiffany Chau <102318022+tiffanynchau@users.noreply.github.com>
    Co-authored-by: Jack Yuan <100592057+jackyuan1@users.noreply.github.com>
    Co-authored-by: Cedar Cooper <87053156+CedarCooper@users.noreply.github.com>

--- __tests__/ImageTab.test.js
+++ __tests__/ImageTab.test.js
@@ -5,6 +5,9 @@
 import React from 'react';
 import { describe, expect, test, jest } from '@jest/globals';
 import Images from '../src/components/tabs/Images';
+// import ImageUsers from '../src/components/tabs/ImagesUser';
+
+import mockAxios from 'axios';
 import '@testing-library/react';
 import '@testing-library/jest-dom';
 import {
@@ -14,7 +17,6 @@ import {
   render,
   screen,
 } from '@testing-library/react';
-import { remove } from '../src/components/helper/commands';
 
 const props = {
   imagesList: [
@@ -24,20 +26,18 @@ const props = {
       Name: 'redis',
     },
   ],
-  // repo: ['repo'],
-  run: jest.fn(),
+  runIm: jest.fn(),
+  removeIm: jest.fn(),
 };
 
-// Debug testing, checks for image box to render on screen
-describe('Images', () => {
-  it('renders an image', () => {
-    render(<Images {...props} />);
-  });
-});
+/* ----- mock functions ----- */
+
+// jest.mock('axios');
+// const mockRun = jest.fn();
+// const mockRremove = jest.fn();
 
 /* ----- button testing ------ */
 
-// currently gets stuck at window.runExec method --> reads undefined
 describe('run button on click', () => {
   it('fires run button functionality', async () => {
     const { container } = render(<Images {...props} />);
@@ -57,7 +57,6 @@ describe('pull button on click', () => {
   });
 });
 
-// currently gets stuck at window.runExec method --> reads undefined
 describe('remove button on click', () => {
   it('fires remove button functionality', async () => {
     const { container } = render(<Images {...props} />);
@@ -71,80 +70,15 @@ describe('remove button on click', () => {
 
 /* ------ actions/reducers ------ */
 
-// Get Images
-// Refresh Images
-// Remove Images
+describe('Images', () => {
+  it('renders an image if one is found', () => {
+    render(<Images {...props} />);
+  });
+  it('refreshes page if image removed', () => {
+    // need to check that refreshRunning helper function is called
 
-
-
-//   describe('Rendered images', () => {
-//     test('run button works', async () => {
-//       const runButton = document.querySelector('.run-btn');
-//       await fireEvent.click(runButton);
-//       screen.debug();
-//       expect(runButton).toBeCalled;
-//     });
-//   });
-// });
-
-// configure({ adapter: new Adapter() });
-// function shallowSetup() {
-//   const props = {
-//     imagesList: [
-//       {
-//         resp: "node-php-something",
-//         tag: "latest",
-//         imgid: "fc266a46f885",
-//         created: "toady",
-//         size: "234mb",
-//       },
-//     ],
-//   };
-//   const enzymeWrapper = shallow(<Images {...props} />);
-//   return {
-//     props,
-//     enzymeWrapper,
-//   };
-// }
-// describe("Shallow all of the properties of the Images", () => {
-//   const { enzymeWrapper, props } = shallowSetup();
-//   it("Should render <div> tag in Images", () => {
-//     expect(enzymeWrapper.type()).toEqual("div");
-//     expect(
-//       enzymeWrapper.find("div.renderContainers").find("div").length
-//     ).toEqual(8);
-//   });
-//   it("Should render <h1> tag in Images with a title Images", () => {
-//     expect(enzymeWrapper.containsMatchingElement(<h1>Images</h1>)).toBe(
-//       true
-//     );
-//     expect(enzymeWrapper.find(".tabTitle").text()).toEqual("Images");
-//   });
-//   it("Should render a div tag called runByImage and display all of the properties", () => {
-//     expect(enzymeWrapper.find("div.runByButton").find("button").length).toEqual(
-//       1
-//     );
-//     expect(enzymeWrapper.find("div.runByButton").find("label").length).toEqual(
-//       1
-//     );
-//     expect(enzymeWrapper.find("div.runByButton").find("span").length).toEqual(
-//       1
-//     );
-//     expect(enzymeWrapper.find("div.runByButton").find("input").length).toEqual(
-//       1
-//     );
-//   });
-//   it(`render a div with a class name "containers" and all of it properties`, () => {
-//     expect(enzymeWrapper.find("div.containers"));
-//     expect(enzymeWrapper.find("div.box").find("div").length).toEqual(4);
-//     expect(enzymeWrapper.find("div.box-label").find("h3").length).toEqual(1);
-//     expect(enzymeWrapper.find("div.box-label").find("p").length).toEqual(1);
-//     expect(enzymeWrapper.find("div.stopped-info").find("li").length).toEqual(2);
-//     expect(
-//       enzymeWrapper.find("div.stopped-button").find("button").length
-//     ).toEqual(2);
-//   });
-// });
+  });
+});
 
 //* Dummy Test
 describe('dummy test', () => {
---
+++ __tests__/UsersTab.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { describe, expect, test, jest } from '@jest/globals';
+import '@testing-library/react';
+import '@testing-library/jest-dom';
+import {
+  fireEvent,
+  getByLabelText,
+  getByTestId,
+  render,
+  screen,
+} from '@testing-library/react';
+import Users from '../src/components/tabs/Users';
+import NewUserDisplay from '../src/components/display/NewUserDisplay';
+import { checkPasswordLength } from '../src/components/helper/newUserHelper';
+
+/* ----- Manage Users Table ----- */
+
+// checking for button functionality
+// describe('Manage Users Table', () => {
+//   test()
+// })
+
+/* ----- Create a New User ----- */
+
+describe('Create a New User functionality', () => {
+  // render NewUserDisplay
+  beforeAll(() => {
+    render(<NewUserDisplay />);
+  });
+  // input fields take in text
+  test('input fields take in text', () => {
+    const input = document.getElementById('signupEmail');
+    screen.debug();
+    expect(input).toHaveValue('');
+  });
+  // password is at least 6 chars long
+  test('password field must have at least 6 chars', () => {
+    const password = '123456';
+    const passInput = document.getElementById('signupPassword');
+    expect(passInput).toHaveValue(password).toBeGreaterThanOrEqual(6);
+  });
+  // password and confirm passwords match
+  // if either of these tests fail, check for error message alert
+
+  // submit button on click works
+
+  // Manage Users Table should gain a row
+});

32acd4e6787e9fd56e61118daf935ab1abb5fab0
Drew Manley <andrewbmanley@gmail.com>
2022-11-25 17:24:37 -0600

    more users testing

--- __tests__/UsersTab.test.js
+++ __tests__/UsersTab.test.js
@@ -8,10 +8,22 @@ import {
   getByTestId,
   render,
   screen,
+  waitFor,
 } from '@testing-library/react';
 import Users from '../src/components/tabs/Users';
 import NewUserDisplay from '../src/components/display/NewUserDisplay';
 import { checkPasswordLength } from '../src/components/helper/newUserHelper';
+import { userInfo } from 'os';
+import user from '@testing-library/user-event';
+
+const props = {
+  onSubmit: jest.fn(),
+  onChange: jest.fn(),
+  onClick: jest.fn(),
+}
+
+
+/* ----- Mock Functions ----- */
 
 /* ----- Manage Users Table ----- */
 
@@ -24,13 +36,13 @@ import { checkPasswordLength } from '../src/components/helper/newUserHelper';
 
 describe('Create a New User functionality', () => {
   // render NewUserDisplay
-  beforeAll(() => {
-    render(<NewUserDisplay />);
+  beforeEach(() => {
+    render(<NewUserDisplay {...props} />);
   });
   // input fields take in text
   test('input fields take in text', () => {
     const input = document.getElementById('signupEmail');
-    screen.debug();
+    // screen.debug();
     expect(input).toHaveValue('');
   });
   // password is at least 6 chars long
@@ -40,7 +52,23 @@ describe('Create a New User functionality', () => {
   // password and confirm passwords match
   // if either of these tests fail, check for error message alert
 
-  // submit button on click works
-
+  // submit button works when password fields match
+  test('onSubmit is called when password fields match', async () => {
+    user.type(getPasswordField(), '123456');
+    user.type(getConfirmationField(), '123456');
+    const submitBtn = screen.getByRole('button', { name: 'submit' });
+    await user.click(submitBtn);
+    screen.debug();
+    expect(submitBtn).toBeCalled();
+  })
   // Manage Users Table should gain a row
+
 });
+
+const getPasswordField = () => {
+  return document.getElementById('signupPassword');
+};
+
+const getConfirmationField = () => {
+  return document.getElementById('signupPasswordConfirmation');
+}

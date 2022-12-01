fa779afab3bf6578ef83775fbf122d655db0e334
Sarah <14sbethm@gmail.com>
2022-12-01 12:08:40 -0600

    fixing final issues with typing
    Co-authored-by: Sarah Moosa <74516787+Sbethm@users.noreply.github.com>
    Co-authored-by: Tiffany Chau <102318022+tiffanynchau@users.noreply.github.com>
    Co-authored-by: Jack Yuan <100592057+jackyuan1@users.noreply.github.com>
    Co-authored-by: Cedar Cooper <87053156+CedarCooper@users.noreply.github.com>
    Co-authored-by: Drew Manley <101421661+DrewManley@users.noreply.github.com>

--- src/components/views/viewsTypes.ts
+++ src/components/views/viewsTypes.ts
@@ -1,75 +1,96 @@
-// this file contains the interfaces for each file. Please search a fil by name in order to find what interfaces it uses.
+// this file contains the interfaces for each file in the views folder. 
 
+// for container's being run
 export interface ContainerObj {
-    BlockIO: string, 
-    CPUPerc: string,
-    Container: string,
-    ID: string,
-    MemPerc: string,
-    MemUsage: string,
-    Name: string,
-    NetIO: string,
-    PIDs: string
+  BlockIO: string, 
+  CPUPerc: string,
+  Container: string,
+  ID: string,
+  MemPerc: string,
+  MemUsage: string,
+  Name: string,
+  NetIO: string,
+  PIDs: string,
+  Image?: string,
+  RunningFor?: string
 }
 
+// for container's being stopped
 export interface StoppedContainerObj {
-    Command: string,
-    CreatedAt: string,
-    ID: string,
-    Image: string,
-    Labels: string,
-    LocalVolumes: string,
-    Mounts: string,
-    Names: string,
-    Networks: string,
-    Ports: string,
-    RunningFor: string,
-    Size: string,
-    State: string,
-    Status: string
+  Command: string,
+  CreatedAt: string,
+  ID: string,
+  Image: string,
+  Labels: string,
+  LocalVolumes: string,
+  Mounts: string,
+  Names: string,
+  Networks: string,
+  Ports: string,
+  RunningFor: string,
+  Size: string,
+  State: string,
+  Status: string
 }
 
 export interface ImageObj {
-    imgid: string,
-    reps: string,
-    size: string,
-    tag: string
+  imgid: string,
+  reps: string,
+  size: string,
+  tag: string
 }
 
 export interface UserObj {
-    contact_pref: null | string,
-    container_stops: true | false,
-    cpu_threshold: number,
-    email: string,
-    mem_threshold: number,
-    password: string,
-    phone: string
-    role: string,
-    role_id: number,
-    token: string,
-    username: string,
+  contact_pref: null | string,
+  container_stops: true | false,
+  cpu_threshold: number,
+  email: string,
+  mem_threshold: number,
+  password: string,
+  phone: string
+  role: string,
+  role_id: number,
+  token: string,
+  username: string,
     _id: number
 }
 
 export interface NetworkObj {
-    CreatedAt: string,
-    Driver: string,
-    ID: string,
-    IPv6: string,
-    Internal: string,
-    Labels: string, 
-    Name: string,
-    Scope: string
+  CreatedAt: string,
+  Driver: string,
+  ID: string,
+  IPv6: string,
+  Internal: string,
+  Labels: string, 
+  Name: string,
+  Scope: string
 }
 
 export interface VolumeObj {
-    vol_name: string,
-    containers: object[]
+  vol_name: string,
+  containers: object[]
 }
 
+interface session {
+  _id: string,
+  username: string,
+  email: string,
+  phone: string,
+  role: string,
+  role_id: string,
+  contact_pref: string,
+  mem_threshold: string,
+  cpu_threshold: string,
+  container_stops: string,
+  token: string,
+  isLoggedIn: boolean,
+  userList: any[]
+}
+
+// "any" has been used below since strict typing was used to define these props in the tabs file 
 interface containersList {
-    runningList: RunningListType[],
-    stoppedList: StoppedListType[]
+  runningList: any[],
+  stoppedList: any[]
 }
 
 interface imagesList {
@@ -82,14 +103,16 @@ interface volumeList {
 }
 
 interface notificationList {
-  phoneNumber: any[],
+  phoneNumber: string[],
   memoryNotificationList: any[],
-  cpuNotificationList: any[],stoppedNotificationList: any[],
+  cpuNotificationList: any[],
+  stoppedNotificationList: any[],
 }
 
 export interface StateType {
   containersList: containersList,
   images: imagesList,
-  volumeList: volumeList,
-  notificationList: notificationList
+  notificationList: notificationList,
+  session: session,
+  volumeList: volumeList
 };

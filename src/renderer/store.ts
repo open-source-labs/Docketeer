023b5bb0e8d52c47e1cb3bc3111cba68f1389907
Cedar Cooper <cedar.m.cooper@gmail.com>
2022-12-13 13:39:14 -0800

    Fixed metrics and store file.
    Co-authored-by: Sarah Moosa <74516787+Sbethm@users.noreply.github.com>
    Co-authored-by: Tiffany Chau <102318022+tiffanynchau@users.noreply.github.com>
    Co-authored-by: Jack Yuan <100592057+jackyuan1@users.noreply.github.com>
    Co-authored-by: Cedar Cooper <87053156+CedarCooper@users.noreply.github.com>
    Co-authored-by: Drew Manley <101421661+DrewManley@users.noreply.github.com>

--- src/renderer/store.ts
+++ src/renderer/store.ts
@@ -3,16 +3,12 @@ import reducers from '../redux/reducers/index';
 
 const store = configureStore({
   reducer: reducers,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    immutableCheck: false,
+    serializableCheck: false,
+})
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export default store;
-
-
-
-

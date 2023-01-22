a70e906ca24922debec4b51ecfe4e8a8a9f694aa
Garima Bhatia <marigolddancestudio@gmail.com>
2023-01-22 11:43:25 -0500

    will fix general styling, nav bar ui, users-table ui in users tab, spacing in processLogs

--- src/components/tabs/ProcessLogs.js
+++ src/components/tabs/ProcessLogs.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import ProcessLogsCard from '../display/ProcessLogsCard';
+import React from 'react'
+import ProcessLogsCard from '../display/ProcessLogsCard'
 
 /**
  * Display all running and stopped containers. Each box can be selected to view process log tables.
@@ -9,42 +9,51 @@ import ProcessLogsCard from '../display/ProcessLogsCard';
  */
 
 const ProcessLogs = (props) => {
-  const renderRunningList = [];
+  const renderRunningList = []
   props.runningList.map((container, index) => {
     renderRunningList.push(
       <ProcessLogsCard
         key={index}
         index={index}
         container={container}
-        status='Running'
-      />
-    );
-  });
+        status="Running"
+      />,
+    )
+  })
 
-  const renderStoppedList = [];
+  const renderStoppedList = []
   props.stoppedList.map((container, index) => {
     renderStoppedList.push(
-      <ProcessLogsCard key={index} index={index} container={container} status='Stopped' />
-    );
-  });
+      <ProcessLogsCard
+        key={index}
+        index={index}
+        container={container}
+        status="Stopped"
+      />,
+    )
+  })
 
   return (
-    <div className='renderContainers'>
-      <div className='header'>
-        <h1 className='tabTitle'>Process Logs</h1>
+    <div className="renderContainers">
+      <div className="header">
+        <h1 className="tabTitle">Process Logs</h1>
       </div>
 
-      <h3>Running Containers: {props.runningList.length}</h3>
+      <h3 className="container-heading">
+        Running Containers: {props.runningList.length}
+      </h3>
 
-      <div className='containers'>{renderRunningList}</div>
+      <div className="containers">{renderRunningList}</div>
 
       <br></br>
 
-      <h3>Stopped Containers: {props.stoppedList.length}</h3>
+      <h3 className="container-heading">
+        Stopped Containers: {props.stoppedList.length}
+      </h3>
 
-      <div className='containers'>{renderStoppedList}</div>
+      <div className="containers">{renderStoppedList}</div>
     </div>
-  );
-};
+  )
+}
 
-export default ProcessLogs;
+export default ProcessLogs

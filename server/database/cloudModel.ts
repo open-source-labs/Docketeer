c0c500e061f3c90bff845d714b1ee17269db0196
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-03-06 02:04:36 -0500

    Removed unnecessary env variables and updated dashboard json to its most recent state

--- server/database/cloudModel.ts
+++ server/database/cloudModel.ts
@@ -1,18 +1,17 @@
 import { Pool } from 'pg';
 import { SqlQuery } from '../../types';
 import {
+  POSTGRES_SERVICE,
   POSTGRES_USER,
   POSTGRES_PASS,
   POSTGRES_NAME,
-  POSTGRES_PORT,
-  POSTGRES_SERVICE,
 } from '../../config.js';
 
 const host = POSTGRES_SERVICE;
 const user = POSTGRES_USER;
 const password = POSTGRES_PASS;
 const database = POSTGRES_NAME;
-const port = POSTGRES_PORT;
+const port = 5432;
 const pool = new Pool({
   host,
   user,

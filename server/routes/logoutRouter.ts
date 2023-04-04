42a5880fd164657525192cde598806eecaa2fcc9
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-04-04 01:03:15 -0400

    Updated comment on logout route with new guidance.

--- server/routes/logoutRouter.ts
+++ server/routes/logoutRouter.ts
@@ -3,15 +3,11 @@
  * @description Routes all requests to logout endpoint
  */
 import { Router, Request, Response } from 'express';
-// import dbController from '../controllers/dbController';
-// import userController from '../controllers/userController';
 const router = Router();
-// Note: we remove a token, though tokens are added in bcryptController.hashCookie and hashCookie mw is never used in any of our routes
 
 // ==========================================================
 // Route: /
-// Purpose: Removes token (sets token to null) after user logs out.
-// Note: tokens and cookies not currently implemented
+// Purpose: Removes jwt after user clicks log out. Should implement sessions in the future.
 // ==========================================================
 router.post('/', (req: Request, res: Response): Response => {
   res.clearCookie('admin', { httpOnly: true });

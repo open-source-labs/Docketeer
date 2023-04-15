f85eb5eac7013cfb9532850add84dadf3e72b156
jaenixlee <jaeni_lee@yahoo.com>
2023-04-15 11:52:12 -0400

    added toggle button for metrics

--- src/components/Metrics/Metrics.tsx
+++ src/components/Metrics/Metrics.tsx
@@ -3,7 +3,7 @@ import styles from './Metrics.module.scss';
 
 const Metrics = (): JSX.Element => {
   const [kubernetesOrNah, setFrame] = useState(1);
-  const button = kubernetesOrNah === 1 ? 'Containers' : 'Kubernetes Cluster';
+  const button = kubernetesOrNah === 1 ? 'Container Metrics' : 'Kubernetes Cluster Metrics';
 
   const handleToggle = () => {
     setFrame((prevFrame) => (prevFrame === 1 ? 2 : 1));
@@ -12,9 +12,7 @@ const Metrics = (): JSX.Element => {
   return (
     <div className={styles.wrapper}>
       <h2>METRICS DASHBOARD</h2>
-      <button className={styles.button} onClick={handleToggle}>
-        {button}
-      </button>
+      <input type="checkbox" id="switch" onClick={handleToggle} />{button}<label htmlFor="switch">{button}</label>
       {kubernetesOrNah === 1 ? (
         <iframe
           className={styles.metrics}
@@ -23,7 +21,7 @@ const Metrics = (): JSX.Element => {
       ) : (
         <iframe
           className={styles.metrics}
-          src="http://localhost:3000/d/Hkev6ZP4z/node-exporter-nodes?orgId=1&refresh=30s"
+          src="http://localhost:3000/d/ldxep7PVz/test?orgId=1&refresh=10s&kiosk"
         />
       )}
     </div>

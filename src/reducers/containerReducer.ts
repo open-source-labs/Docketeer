55deaae7a0a637a700cd629c143d19c7ab48df0c
Michael Liu <micliu@ucdavis.edu>
2023-06-26 23:45:33 -0700

    Edit Documentation of RTK

--- src/reducers/containerReducer.ts
+++ src/reducers/containerReducer.ts
@@ -5,17 +5,6 @@ import {
   StoppedListType,
 } from '../../types';
 
-
-// createSlice is the main function used to define the redux logic
-// PayloadAction is a TS type that represents 'this is the contents of one given action object'
-
-
-// Below represents the shape of the state inside of our slice that is managed by the reducer
-// if there was an interface
-
-
-
-// below are defining the initial state
 const initialState: ContainerStateType = {
   runningList: [],
   stoppedList: [],
@@ -23,23 +12,11 @@ const initialState: ContainerStateType = {
   composeStack: [],
 };
 
-// Now we define the Slice that contains the reducer logic
 export const containerSlice = createSlice({
   name: 'containers',
   initialState,
-
-  // Below is where we define the different types of updates that we are going to have inside this reducer
   reducers: {
-    // below is an inline object function
-    
     stopRunningContainer: (state, action: PayloadAction<string>) => {
-      // in a normal redux reducer, we would return (...state) to make a copy of state for an immutable update and then we would overwrite the value field
-      // RTK simplifies the process, so rather than previous stuff, we can write what looks like mutating code, directly into this reducer. We don't even need a return keyword
-      // With RTK, it uses a library called immir and immir wraps our state updates and it tracks all our
-      // mutations that we tried to do, and once we are done it actually kind of replays them and it turns it
-      // into a safe and correct mutable update as if we did all the copying and spreading and mapping
-      //
-
       state.runningList.filter((container) => container.ID !== action.payload);
     },
     runStoppedContainer: (state, action: PayloadAction<string>) => {
@@ -63,15 +40,7 @@ export const containerSlice = createSlice({
   },
 });
 
-// Typically with redux, you see these things called action creators, and the idea is that it is a function
-// that makes and returns an action object, typically you write those by hand
-// createSlice actually just made one of those for us.
-// The slice object has a couple of different fields in it
-// One is a reducer function that knows how to update the state for all the different cases we encounter,
-// It also has generated an action creator for each of the different functions inside the reducers field
-//
 
-// if we mouse over any of the below, TS says it is an ActionCreatorWithAnOptionalPayload
 export const {
   stopRunningContainer,
   runStoppedContainer,
@@ -83,7 +52,3 @@ export const {
 export default containerSlice.reducer;
 
 
-// All of the above is the basic shape of what we call a redux slice
-// Use the createSlice API to find what your state, one or more reducers to apply updates, export action creators
-// export the reducer that handles all those cases/
-// Now we need to setup our Redux Store, which is a one time thing for our app, store.ts
--- src/store.ts
+++ src/store.ts
@@ -8,23 +8,8 @@ import userReducer from './reducers/userReducer';
 import sessionReducer from './reducers/sessionReducer';
 import volumeReducer from './reducers/volumeReducer';
 
-// Above we import a function called configureStore from '@reduxjs/toolkit'
-// This is a wrapper around the basic redux createStore function, it takes your reducer and makes a store
-// but it also automatically sets up a store with the right defaults
-// i.e. it turns on Redux DevTools extension, automatically adds the Thunk middleware,
-// automatically turns on a couple of development checks that catch common mistakes like accidental mutations
-// now we have to import our containerReducer, it's the default so no curly braces
-//
 const store = configureStore({
-
-  // You could pass in an entire reducer function here, but remember how redux has that combine reducers function that lets you mash a bunch of reducers together? 
-  // configureStore will do that for us automatically if we pass in an object,
-  // so we pass in another object called reducer
-  //
   reducer: {
-    // field container with a value of containerReducer, this will automatically call combineReducer
-    // so we end up with a state.containers field in our state
-    //
     containers: containerReducer,
     images: imageReducer,
     composes: composeReducer,
@@ -41,24 +26,7 @@ const store = configureStore({
     }),
 });
 
-
-
-
-
-// we are going to export some types based on our store itself below
-// this means that we are taking the store's dispatch function and we are asking TS,
-// what is this thing? well it is a function that takes certain kinds of arguments
-// and we are exporting the type of that function as a thing we can use, 
 export type AppDispatch = typeof store.dispatch;
-
-
-// Also export a type called RootState, and that equals... TS has a built in type called 'ReturnType'
-// Mouse over RootState and check, oh look it's an object that has a field called containers that is the 
-// containers state. There is nothing specific to RTK about this, it's using TS inference to figure out 
-// as much as possible so we do not have to declare it ourselves.
-// so if we add more slice Reducers to our store, that type updates automatically.
-// There are now two more setup steps needed.
-// go over to index.tsx to setup our React Redux Provider so the rest of the app can have access to the store
 
 // grabbing type of state returned from the reducer and setting that equal to RootState
 // RootState is used to dynamically change the type of the hooks

4dd4c2c72f9b6f8f309619918737c450d7f44cb0
John Kim <jayoo0621@gmail.com>
2023-07-18 14:18:31 -0700

    add commets in NetworkListModal component co-author : adriankormier <adriankormier@gmail.com> co-author : KwonJiyongGD <micliu@ucdavis.edu> co-author : garrettallen0 <Garrett.allen13@gmail.com> co-author : shao-shuai <shuai-shao@outlook.com>

--- src/components/NetworkListModal/NetworkListModal.tsx
+++ src/components/NetworkListModal/NetworkListModal.tsx
@@ -16,21 +16,31 @@ const NetworkListModal = ({
   closeNetworkList,
   networkContainerList,
 }: any): JSX.Element => {
+  
+  // useEffect that sets up an event listener to track scroll position changes and saves them in the 'scrollPositionModule'.
   useEffect(() => {
+    // This function 'handleScroll' is called whenever the user scrolls the window.
     const handleScroll = () => {
+      // Get the current vertical scroll position of the window.
       const scrollY = window.scrollY;
+      // Call the 'setScrollPosition' function from 'scrollPositionModule' to update the stored scroll position.
       scrollPositionModule.setScrollPosition(scrollY);
     };
-
+    // Add the 'handleScroll' function as a scroll event listener on the window.
     window.addEventListener('scroll', handleScroll);
+    // Cleanup function that runs when the component is unmounted, and it removes the event listener to avoid memory leaks.
     return () => {
+      // Remove the 'handleScroll' function as a scroll event listener from the window.
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
+  // useEffect that retrieves the previously saved scroll position from the 'scrollPositionModule' and applies it to the window's scroll position.
   useEffect(() => {
+    // Get the previously stored scroll position from 'scrollPositionModule'.
     const savedScrollPosition = scrollPositionModule.getScrollPosition();
     if (savedScrollPosition !== null) {
+      // scroll the window to the retrieved scroll position, effectively restoring the previous scroll position if there's savedScrollPosition available
       window.scrollTo(0, savedScrollPosition);
     }
   }, []);
@@ -68,7 +78,7 @@ const NetworkListModal = ({
             className={globalStyles.buttonSmall}
             onClick={() => closeNetworkList()}
           >
-               CLOSE
+            CLOSE
           </button>
         </div>
       </div>

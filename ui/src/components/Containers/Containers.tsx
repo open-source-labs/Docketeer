43333c215f528befeedf536a817ac197ffef4b67
thejohnny5 <johnny5jg5@gmail.com>
2023-09-19 13:07:53 -0400

    Changed localhost:2999 to localhost:49155

--- ui/src/components/Containers/Containers.tsx
+++ ui/src/components/Containers/Containers.tsx
@@ -6,7 +6,6 @@ import useHelper from '../../helpers/commands';
 import { createAlert, createPrompt } from '../../reducers/alertReducer';
 import styles from './Containers.module.scss';
 import ContainersCard from '../ContainersCard/ContainersCard';
-import { createDockerDesktopClient } from '@docker/extension-api-client';
 
 
 /**
@@ -84,7 +83,7 @@ const Containers = (): JSX.Element => {
       )
     );
   };
-  const ddClient = createDockerDesktopClient();
+
   return (
     <div>
       <div className={styles.wrapper}>
@@ -93,9 +92,9 @@ const Containers = (): JSX.Element => {
     
           <div className={styles.toggle}>
             <div>
-              {activeButton === 1 && <iframe src='http://localhost:2999/d-solo/h5LcytHGz/system?orgId=1&refresh=10s&panelId=75' width="100%"></iframe>}
-              {activeButton === 2 && <iframe src="http://localhost:2999/d-solo/h5LcytHGz/system?orgId=1&refresh=10s&panelId=7" width="100%"></iframe>}
-              {activeButton === 3 && <iframe src="http://localhost:2999/d-solo/h5LcytHGz/system?orgId=1&refresh=10s&panelId=8" width="100%"></iframe>}
+              {activeButton === 1 && <iframe src="http://localhost:49155/d-solo/h5LcytHGz/system?orgId=1&refresh=10s&panelId=75" width="100%"></iframe>}
+              {activeButton === 2 && <iframe src="http://localhost:49155/d-solo/h5LcytHGz/system?orgId=1&refresh=10s&panelId=7" width="100%"></iframe>}
+              {activeButton === 3 && <iframe src="http://localhost:49155/d-solo/h5LcytHGz/system?orgId=1&refresh=10s&panelId=8" width="100%"></iframe>}
             </div>
             <div className={styles.buttons}>
               <button className={activeButton === 1 ? styles.active : styles.notActive} onClick={() => setActiveButton(1)}>Memory</button>

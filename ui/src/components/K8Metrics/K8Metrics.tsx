41d9fe375a21b29bb4265f196f11208362d029cf
thejohnny5 <johnny5jg5@gmail.com>
2023-09-25 13:24:31 -0400

    Added metrics to grafana and prometheus

--- ui/src/components/K8Metrics/K8Metrics.tsx
+++ ui/src/components/K8Metrics/K8Metrics.tsx
@@ -9,7 +9,7 @@ const K8Metrics = (): JSX.Element => {
       <h2>Kuburnetes Cluster Health Metrics</h2>
       <div>
         <iframe className={styles.metrics}
- src="http://localhost:49155/d/k8s_views_pods/kubernetes-views-pods?orgId=1&refresh=30s&var-datasource=PBFA97CFB590B2093&var-namespace=&var-pod=&var-resolution=30s" frameborder="0"></iframe>
+ src="http://localhost:49155/d/k8s_views_pods/kubernetes-views-pods?orgId=1&refresh=30s&var-datasource=PBFA97CFB590B2093&var-namespace=&var-pod=&var-resolution=30s" frameBorder="0"></iframe>
       </div>
     </div>
   )

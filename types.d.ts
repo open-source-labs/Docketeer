502a9f403030a648c1850add53ffc99636b04648
thejohnny5 <johnny5jg5@gmail.com>
2023-09-28 09:26:37 -0400

    Separated type declaration files

---
+++ types.d.ts
@@ -0,0 +1,22 @@
+export interface ContainerPS {
+  ID: string;
+  Command?: string;
+  CreatedAt: string;
+  Image?: string;
+  Labels?: string[];
+  LocalVolumes?: string;
+  Mounts?: string;
+  Names?: string;
+  Networks?: string[];
+  Ports?: string[];
+  RunningFor?: string;
+  Size?: string;
+  State: string; //tells if running
+  Status?: string;
+}
+
+export interface LogObject {
+  timeStamp: string;
+  logMsg: string;
+  containerName: string;
+}

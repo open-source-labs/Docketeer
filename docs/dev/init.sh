fe0099f738925fe1df1ed85aa1e7405173e3dd4f
thejohnny5 <johnny5jg5@gmail.com>
2023-10-11 11:50:38 -0400

    Added assets for docketeer config

--- docs/dev/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm install my-prometheus prometheus-community/prometheus
-kubectl port-forward service/my-prometheus-server 45555:80

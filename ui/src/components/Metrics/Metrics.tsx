43440045e316756bf872f9130978bf8e127c32ff
Cristian Morales <crismorales@protonmail.com>
2023-10-10 20:52:10 -0500

    resolved merge conflicts

--- ui/src/components/Metrics/Metrics.tsx
+++ ui/src/components/Metrics/Metrics.tsx
@@ -3,26 +3,9 @@ import styles from './Metrics.module.scss';
 
 const Metrics = (): JSX.Element => {
   const [resetIframe, setResetIframe] = useState<boolean>(true);
-  const [timeRange, setTimeRange] = useState<string>('&from=now-2h&to=now');
-  const timeDictionary = {
-    Seconds: 's',
-    Minutes: 'm',
-    Hours: 'h',
-    Days: 'd',
-    Months: 'M',
-    Years: 'y',
-  };
 
   const handleHome = (): void => {
     setResetIframe(resetIframe ? false : true);
-  };
-  // Last {number} {dictionary}, drop down menu, with a default nume slot, an time range
-  // pretty much just the graf component page minute the absolute time range power
-  // default to last 2 hours
-  //
-
-  const handleTimeRangeChange = (): void => {
-    console.log('change')
   };
 
   return (
@@ -40,13 +23,7 @@ const Metrics = (): JSX.Element => {
           key={resetIframe}
           id='iframe'
           className={styles.metrics}
-<<<<<<< HEAD
-          src={
-            `http://localhost:49155/d/metrics_monitoring/docker-and-system-monitoring?orgId=1&refresh=5s&kiosk${timeRange}`
-          }
-=======
           src='http://localhost:49155/d/metrics_monitoring/docker-and-system-monitoring?orgId=1&refresh=10s&kiosk'
->>>>>>> dev
         />
       </div>
     </div>

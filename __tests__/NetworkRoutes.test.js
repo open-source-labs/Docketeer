7141f6c872b720e797df43ec7f1f9d9ab69dea40
peter chung <peter.chung.716@gmail.com>
2023-10-24 19:34:56 -0400

    fix networkRouter testing

--- __tests__/NetworkRoutes.test.js
+++ __tests__/NetworkRoutes.test.js
@@ -7,17 +7,33 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 // TODO:fix the router, update the names. api router and commande router only exist in the old version 
-import apiRouter from '../server/routes/apiRouter';
-import commandRouter from '../server/routes/commandRouter';
+// import apiRouter from '../server/routes/apiRouter';
+// import commandRouter from '../server/routes/commandRouter';
 
-app.use('/gapi', apiRouter);
-app.use('/api', apiRouter);
-app.use('/command', commandRouter);
+import networkRouter from '../backend/routers/docker/networkRouter';
 
-describe('/command/networkContainers', () => {
-  test('Get networkContainers', async () => {
+// app.use('/gapi', apiRouter);
+// app.use('/api', apiRouter);
+// app.use('/command', commandRouter);
+
+app.use('/network', networkRouter);
+
+// describe('/command/networkContainers', () => {
+//   test('Get networkContainers', async () => {
+//     const res = await request(app)
+//       .get('/command/networkContainers');
+//     expect(res.status).toBe(200)
+//     expect(res.body).toBeInstanceOf(Array)
+//     res.body.forEach(network => {
+//       expect(network.Name).toBeDefined()
+//     })
+//   });
+// })
+
+describe('/network/', () => {
+  test('Get a list of networks running on Docker', async () => {
     const res = await request(app)
-      .get('/command/networkContainers');
+      .get('/network/');
     expect(res.status).toBe(200)
     expect(res.body).toBeInstanceOf(Array)
     res.body.forEach(network => {
@@ -26,9 +42,21 @@ describe('/command/networkContainers', () => {
   });
 })
 
-describe('/command/networkListContainers', () => {
-  test('Get networkListContainers', async () => {
-    const res = await request(app).get('/command/networkListContainers');
+// describe('/command/networkListContainers', () => {
+//   test('Get networkListContainers', async () => {
+//     const res = await request(app).get('/command/networkListContainers');
+//     expect(res.status).toBe(200)
+//     expect(res.body).toBeInstanceOf(Array)
+//     res.body.forEach(network => {
+//       expect(network.networkName).toBeDefined()
+//       expect(network.containers).toBeDefined()
+//     })
+//   })
+// })
+
+describe('/network/container', () => {
+  test('Get a list of networks with the containers they are attached to', async () => {
+    const res = await request(app).get('/network/container');
     expect(res.status).toBe(200)
     expect(res.body).toBeInstanceOf(Array)
     res.body.forEach(network => {
@@ -38,11 +66,67 @@ describe('/command/networkListContainers', () => {
   })
 })
 
-describe('/command/networkCreate', () => {
+// describe('/command/networkCreate', () => {
+
+//   beforeAll(async () => {
+//     await request(app)
+//       .post('/command/networkCreate')
+//       .send({
+//         networkName: "test2",
+//       })
+//   })
+
+//   afterAll(async () => {
+//   await request(app)
+//       .post('/command/networkRemove')
+//       .send({
+//         networkName: "test1",
+//       })
+//   await request(app)
+//       .post('/command/networkRemove')
+//       .send({
+//         networkName: "test2",
+//       })
+// });
+
+//   test('networkCreate with a valid name', async () => {
+//     const res = await request(app)
+//       .post('/command/networkCreate')
+//       .send({
+//         networkName: "test1",
+//       })
+//     expect(res.status).toBe(200)
+//     expect(res.body.hash).toBeDefined()
+//   });
+
+//   test('networkCreate duplicate', async () => {
+//     const res = await request(app)
+//       .post('/command/networkCreate')
+//       .send({
+//         networkName: "test2",
+//       })
+//     expect(res.status).toBe(200)
+//     expect(res.body.hash).not.toBeDefined()
+//     expect(res.body.error).toBeDefined()
+//   });
+  
+//   test('networkCreate with an invalid name', async () => {
+//     const res = await request(app)
+//       .post('/command/networkCreate')
+//       .send({
+//         networkName: "#test",
+//       })
+//     expect(res.status).toBe(200)
+//     expect(res.body.hash).not.toBeDefined()
+//     expect(res.body.error).toBeDefined()
+//   });
+// })
+
+describe('/network/', () => {
 
   beforeAll(async () => {
     await request(app)
-      .post('/command/networkCreate')
+      .post('/network/')
       .send({
         networkName: "test2",
       })
@@ -50,20 +134,20 @@ describe('/command/networkCreate', () => {
 
   afterAll(async () => {
   await request(app)
-      .post('/command/networkRemove')
+      .delete('/network/:id')
       .send({
         networkName: "test1",
       })
   await request(app)
-      .post('/command/networkRemove')
+      .delete('/network/:id')
       .send({
         networkName: "test2",
       })
-});
+  })
 
-  test('networkCreate with a valid name', async () => {
+  test('create network with a valid name', async () => {
     const res = await request(app)
-      .post('/command/networkCreate')
+      .post('/network/')
       .send({
         networkName: "test1",
       })
@@ -71,9 +155,9 @@ describe('/command/networkCreate', () => {
     expect(res.body.hash).toBeDefined()
   });
 
-  test('networkCreate duplicate', async () => {
+  test('create an invalid duplicate network', async () => {
     const res = await request(app)
-      .post('/command/networkCreate')
+      .post('/network/')
       .send({
         networkName: "test2",
       })
@@ -82,9 +166,9 @@ describe('/command/networkCreate', () => {
     expect(res.body.error).toBeDefined()
   });
   
-  test('networkCreate with an invalid name', async () => {
+  test('create a network with an invalid name', async () => {
     const res = await request(app)
-      .post('/command/networkCreate')
+      .post('/network/')
       .send({
         networkName: "#test",
       })

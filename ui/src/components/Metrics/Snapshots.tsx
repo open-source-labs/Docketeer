7d45ef07a0bafb51ab3763fd0319a500b882ce59
peter chung <peter.chung.716@gmail.com>
2023-10-31 16:24:26 -0400

    create dropdown list of dates

--- ui/src/components/Metrics/Snapshots.tsx
+++ ui/src/components/Metrics/Snapshots.tsx
@@ -3,7 +3,8 @@ import styles from './Metrics.module.scss';
 
 const Snapshots = (): JSX.Element => {
  
-  const dropDown = [];
+  // const [dropDown, updateDropDown] = useState<JSX.Element[]>([])
+  const dropDown: JSX.Element[] = []
   const getDates = () : void => {
     fetch('http://localhost:3000/api/saveMetricsEntry/date')
       .then((data) => data.json())
@@ -13,12 +14,35 @@ const Snapshots = (): JSX.Element => {
       .catch((err) => console.log(err));
   }
   const populateDropdown = (dates: []): void => {
+    const newDropDown: JSX.Element[] = [];
+    dates.forEach((dateObj: any) => {
+      const date: string = dateObj.metric_date
+    dropDown.push(
+        <option>
+          {date}
+        </option>
+      )
+      console.log('date added to dropDown list:', date)
+      console.log('length of dropdown list:', dropDown.length)
+    });
+    // updateDropDown(newDropDown);
   }
 
   getDates();
+  console.log('this is the dropdown after invoking getDates():', dropDown)
+  // useEffect(() => {
+  //     getDates();
+  // },[])
+  // console.log('current dropdown:', dropDown[0])
 
   return (
     <div>
+      <label>
+        Choose a date:
+      </label>
+        <select>
+          {dropDown}
+        </select>
     </div>
   );
 };

15b4f841f4b388c9f611eae96cfcb37c061b90f2
Gabriela Gomez <gabspg@gmail.com>
2023-11-01 15:36:18 -0400

    Able to compare Snapshots on Snapshots page

--- backend/routers/saveMetricsRouter.ts
+++ backend/routers/saveMetricsRouter.ts
@@ -17,24 +17,31 @@ router.post(
     const { date, diskSpace, memory, swap, CPU_usage, available_Memory } = await req.body;
     const inputs = [
       date,
-      diskSpace,
-      memory,
-      swap,
-      CPU_usage,
-      available_Memory,
+      diskSpace.result[0].value[1],
+      memory.result[0].value[1],
+      swap.result[0].value[1],
+      CPU_usage.result[0].value[1],
+      available_Memory.result[0].value[1],
     ];
     const queryStr = 'INSERT INTO snapshots ( metric_date, diskSpace, memory, swap, CPU_usage, available_memory) VALUES($1,$2,$3,$4,$5,$6)';
     db.query(queryStr, inputs);
 
-    return res.status(200).json({'req': 'hi'});
+    return res.status(200).json({});
   }
 );
 
 router.get('/date', async (req: Request, res: Response) => {
   const queryStr = 'SELECT metric_date FROM snapshots';
   const dates = await db.query(queryStr);
-  console.log('sending back query, here are the dates', dates.rows)
   return res.status(200).json(dates.rows);
 });
+
+
+router.get('/snapshot/:id', async (req: Request, res: Response) => {
+  const { id } = req.params
+  const queryStr = `SELECT metric_date, diskSpace, memory, swap, CPU_usage, available_memory FROM snapshots WHERE metric_date='${id}'`
+  const result = await db.query(queryStr)
+  return res.status(200).json(result.rows[0]);
+})
 
 export default router;
--- ui/src/components/Metrics/Snapshots.tsx
+++ ui/src/components/Metrics/Snapshots.tsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import styles from './Metrics.module.scss';
 
 const Snapshots = (): JSX.Element => {
- 
+
+  const [snapshotLeft, updateSnapshotLeft] = useState<JSX.Element[]>([]);
+  const [snapshotRight, updateSnapshotRight] = useState<JSX.Element[]>([]);
   const [dropDown, updateDropDown] = useState<JSX.Element[]>([])
-  // const dropDown: JSX.Element[] = []
+
   const getDates = () : void => {
     fetch('http://localhost:3000/api/saveMetricsEntry/date')
       .then((data) => data.json())
@@ -18,7 +20,7 @@ const Snapshots = (): JSX.Element => {
     dates.forEach((dateObj: any) => {
       const date: string = dateObj.metric_date
     newDropDown.push(
-        <option>
+        <option id="date-select" value={date}>
           {date}
         </option>
       )
@@ -28,19 +30,68 @@ const Snapshots = (): JSX.Element => {
     updateDropDown(newDropDown);
   }
 
-  // getDates();
+  const retrieveSnapshot = (dropDownSide:string) => {
+    const valueSelected: any = document.querySelector(`#select-${dropDownSide}`);
+    const optionValue = valueSelected ? valueSelected.value : null;
+    console.log('valueSelected', optionValue);
+
+    getSnapshotDB(optionValue, dropDownSide);
+  }
+
+  const getSnapshotDB = (date, dropDownSide:string) => {
+    fetch(`http://localhost:3000/api/saveMetricsEntry/snapshot/${date}`)
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data)
+        displaySnapshot(data, dropDownSide);
+      })
+      .catch((err) => console.log(err));
+  }
+  
+  const displaySnapshot = (metrics, dropDownSide) => {
+    const dataArray: JSX.Element[] = [];
+    for (const key in metrics) {
+      dataArray.push(
+        <label>
+          <p>
+            {key}: {metrics[key]}
+          </p>
+        </label>
+      );
+    }
+    if (dropDownSide === 'left') {
+      updateSnapshotLeft(dataArray);
+    }
+    else if (dropDownSide === 'right'){
+      updateSnapshotRight(dataArray);
+    }
+  }
+
   useEffect(() => {
       getDates();
   },[])
 
   return (
-    <div>
-      <label>
-        Choose a date:
-      </label>
-        <select>
-          {dropDown}
-        </select>
+    <div className={styles.wrapper}>
+      <label>Choose a date:</label>
+      <select id="select-left">{dropDown}</select>
+      <button
+        className={styles.button}
+        onClick={() => retrieveSnapshot('left')}
+      >
+        Retrieve Snapshot
+      </button>
+      {snapshotLeft}
+
+      <label>Choose a date:</label>
+      <select id="select-right">{dropDown}</select>
+      <button
+        className={styles.button}
+        onClick={() => retrieveSnapshot('right')}
+      >
+        Retrieve Snapshot
+      </button>
+      {snapshotRight}
     </div>
   );
 };
